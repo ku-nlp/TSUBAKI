423e8bbe4018e458563f8c1043de2a7cecb413fc
Keiji Shinzato <skeiji@(null)>
2006-10-23 13:18:31 +0000

    係り受けインデックスを作成するオプション(-knp)を追加.

--- scripts/make_idx.pl
+++ scripts/make_idx.pl
@@ -9,9 +9,12 @@
 use strict;
 use encoding 'utf8';
 use XML::DOM;
-use Encode qw(decode);
+use Encode qw(decode encode from_to);
 use Getopt::Long;
-my (%opt); GetOptions(\%opt, 'in=s', 'out=s', 'direct');
+use Indexer qw(makeIndexfromJumanResul_utf8 makeIndexfromKnpResult_utf8);
+
+
+my (%opt); GetOptions(\%opt, 'in=s', 'out=s', 'direct', 'knp');
 
 die "Option Error!\n" if (!$opt{in} || !$opt{out});
 
@@ -31,43 +34,73 @@ foreach my $ftmp (sort {$a <=> $b} readdir(DIR)) {
     my $NAME = $1 . $2;
     my $SHORT_NAME = $2;
 
-    print "$NAME\n"; 
+#   print STDERR "$NAME";
     
     # ファイルからJUMANの解析結果を読み込む
     open (FILE, '<:utf8', "$opt{in}/$ftmp") || die("no such file $ftmp\n");
+    if ($opt{knp}) {
+	# KNPの解析結果を使ってインデックスを作成
 
-    # XMLをテキストとして該当部分のみを抽出
-    if ($opt{direct}) {
-	my $flag;
+	my $flag = 0;
+	my $buff = '';
 	while (<FILE>) {
- 	    if (/<\/Juman>/) {
- 		$flag = 0;
- 	    }
- 	    elsif (s/.*\<Juman\>\<\!\[CDATA\[//) {
+	    next if($_ =~ /^succeeded\.$/);
+	    
+
+	    if($_ =~ /\<S.+ Id="(\d+)"\>/){
+		print STDERR "\rdir=$opt{in},file=$NAME (Id=$1)";
+	    }
+
+ 	    if($_ =~ /^\]\]\><\/Knp>/){
+		my $indexes = &Indexer::makeIndexfromKnpResult_utf8(encode('utf8',$buff));
+		foreach my $k (keys %{$indexes}){
+		    $freq{$k} += $indexes->{$k};
+		}
+		$buff = '';
+		$flag = 0;
+ 	    }elsif($_ =~ /.*\<Knp\>\<\!\[CDATA\[/){
+		$buff = "$'";
  		$flag = 1;
- 	    }
-	    &CountData($_) if ($flag);
+ 	    }elsif($flag > 0){
+		$buff .= "$_";
+	    }
 	}
 	close FILE;
+    }else{
+	# XMLをテキストとして該当部分のみを抽出
+	if ($opt{direct}) {
+	    my $flag;
+	    while (<FILE>) {
+		if (/<\/Juman>/) {
+		    $flag = 0;
+		}
+		elsif (s/.*\<Juman\>\<\!\[CDATA\[//) {
+		    $flag = 1;
+		}
+		&CountData($_) if ($flag);
+	    }
+	    close FILE;
+	}
+
+	# XMLをXMLとして解析(デフォルト)  
+	else {
+	    my ($buf);
+	    while (<FILE>) {
+		$buf .= $_;
+	    }
+	    close FILE;
+	    
+	    my $doc = $parser->parse($buf);
+	    &read_sf($doc);
+	    $doc->dispose();   
+	}
     }
 
-    # XMLをXMLとして解析(デフォルト)  
-    else {
-	my ($buf);
-	while (<FILE>) {
-	    $buf .= $_;
-	}
-	close FILE;
-	
-	my $doc = $parser->parse($buf);
-	&read_sf($doc);
-	$doc->dispose();   
-    }
-	
     # 単語IDと頻度のペアを出力
     open (OUT, '>:utf8', "$opt{out}/$NAME.idx");
     &Output(*OUT, $SHORT_NAME);
     close OUT;
+    print STDERR " done.\n";
 }
 closedir(DIR);
 
@@ -106,7 +139,7 @@ sub CountData
     return if ($w[5] =~ /^(形式|副詞的)名詞$/);
     
     my $word = $w[2];
-    if($input =~ /代表表記:(.+)\//){
+    if($input =~ /代表表記:(.+?)\//){
 	$word = $1;
     }
 
