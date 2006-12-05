1e611ed0bd50fc4b4dba5a640ed023c598422c87
Keiji Shinzato <skeiji@(null)>
2006-12-05 06:00:52 +0000

    ウィンドウ幅を指定してインデックスを作成するオプション-windowの追加

--- scripts/make_idx.pl
+++ scripts/make_idx.pl
@@ -12,9 +12,9 @@ use utf8;
 use XML::DOM;
 use Encode qw(decode encode from_to);
 use Getopt::Long;
-use Indexer qw(makeIndexfromJumanResult makeIndexfromKnpResult);
+use Indexer qw(makeIndexfromJumanResult makeIndexfromKnpResult makeIndexfromIndexArray);
 
-my (%opt); GetOptions(\%opt, 'in=s', 'out=s', 'direct', 'knp');
+my (%opt); GetOptions(\%opt, 'in=s', 'out=s', 'direct', 'knp', 'window=s');
 
 die "Option Error!\n" if (!$opt{in} || !$opt{out});
 
@@ -23,44 +23,54 @@ my %freq;
 my $parser = new XML::DOM::Parser unless ($opt{direct});
 my $TAG_NAME = "Juman";
 $TAG_NAME = "Knp" if($opt{knp});
+my $window = -1;
+$window = $opt{window} if($opt{window});
 
 # データのあるディレクトリを開く
 opendir (DIR, $opt{in});
-
 foreach my $ftmp (sort {$a <=> $b} readdir(DIR)) {
     undef %freq;
-	
+
     # *.xmlを読み込む
     # 数字のみのファイルが対象
-    next if ($ftmp !~ /(^0*)(\d+)\.xml$/);
+#   next if ($ftmp !~ /(^0*)(\d+)\.xml$/);
+    next if ($ftmp !~ /(^0*)(\d+)\.xml.gz$/);
     my $NAME = $1 . $2;
     my $SHORT_NAME = $2;
 
-#   print STDERR "$NAME";
-    
-    # ファイルからJUMANの解析結果を読み込む
-    open (FILE, '<:utf8', "$opt{in}/$ftmp") || die("no such file $ftmp\n");
+#   open (FILE, '<:utf8', "$opt{in}/$ftmp") || die("no such file $ftmp\n");
+    open (FILE, "zcat $opt{in}/$ftmp |") || die("no such file $ftmp\n");
+
     # Juman / Knpの解析結果を使ってインデックスを作成
-    
     my $flag = 0;
     my $buff = '';
+    my @index_array;
     while (<FILE>) {
 	next if($_ =~ /^succeeded\.$/);
-	    
+
 	if($_ =~ /\<S.+ Id="(\d+)"\>/){
 	    print STDERR "\rdir=$opt{in},file=$NAME (Id=$1)";
 	}
 
 	if($_ =~ /^\]\]\><\/$TAG_NAME>/){
-#	    my $indexes = &Indexer::makeIndexfromKnpResult(encode('utf8',$buff));
 	    my $indexes;
-#	    my $temp = encode('euc-jp',$buff);
-#	    print STDERR ">>> $temp\n";
-
+	    $buff = decode('utf8', $buff);
 	    if($opt{knp}){
 		$indexes = &Indexer::makeIndexfromKnpResult($buff);
+		if($window > 0){
+		    my $temp = &Indexer::makeIndexArrayfromKnpResult($buff);
+		    foreach my $e (@{$temp}){
+			push(@index_array, $e);
+		    }
+		}
 	    }else{
 		$indexes = &Indexer::makeIndexfromJumanResult($buff);
+		if($window > 0){
+		    my $temp = &Indexer::makeIndexArrayfromJumanResult($buff);
+		    foreach my $e (@{$temp}){
+			push(@index_array, $e);
+		    }
+		}
 	    }
 
 	    foreach my $k (keys %{$indexes}){
@@ -68,14 +78,24 @@ foreach my $ftmp (sort {$a <=> $b} readdir(DIR)) {
 	    }
 	    $buff = '';
 	    $flag = 0;
- 	    }elsif($_ =~ /.*\<$TAG_NAME\>\<\!\[CDATA\[/){
-		$buff = "$'";
- 		$flag = 1;
- 	    }elsif($flag > 0){
-		$buff .= "$_";
-	    }
+	}elsif($_ =~ /.*\<$TAG_NAME\>\<\!\[CDATA\[/){
+	    $buff = "$'";
+	    $flag = 1;
+	}elsif($flag > 0){
+	    $buff .= "$_";
+	}else{
+#	    print encode('euc-jp', $_);
+	}
     }
     close FILE;
+
+    if($window > 0){
+	my $temp = &Indexer::makeIndexfromIndexArray(\@index_array, 30);
+#	my $temp = &Indexer::makeIndexfromIndexArray(\@index_array, $window);
+	foreach my $k (keys %{$temp}){
+	    $freq{$k} += $temp->{$k};
+	}
+    }
 
     # 単語IDと頻度のペアを出力
     open (OUT, '>:utf8', "$opt{out}/$NAME.idx");
