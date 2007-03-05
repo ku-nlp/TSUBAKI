b128e5dd5a80e15b38eb37d48d66b05cd8735404
Keiji Shinzato <skeiji@(null)>
2007-03-05 08:48:09 +0000

    並列句の係り元の係り先を、係り先の係り先にするように変更.

--- scripts/Indexer.pm
+++ scripts/Indexer.pm
@@ -9,9 +9,18 @@ use utf8;
 
 our @EXPORT = qw(makeIndexfromJumanResult makeIndexfromKnpResult makeNgramIndex makeIndexfromIndexArray);
 
+sub new {
+    my($class);
+    my $this = {absolute_pos => -1};
+
+    bless $this;
+}
+
+sub DESTROY {}
+
 ## JUMANの解析結果から索引語を抽出する
 sub makeIndexfromJumanResult{
-    my($juman_result) = @_;
+    my($this,$juman_result) = @_;
     my %index = ();
     my @buff = ();
     my $size = 0;
@@ -53,13 +62,14 @@ sub makeIndexfromJumanResult{
 	    $index{$word}->{pos} = $pos;
 	}
 	$pos++;
+	$this->{absolute_pos}++;
     }
-
+    
     return \%index;
 }
 
 sub makeIndexArrayfromJumanResult{
-    my($juman_result) = @_;
+    my($this,$juman_result) = @_;
     my @buff = ();
     my $size = 0;
     foreach my $line (split(/\n/,$juman_result)){
@@ -248,26 +258,34 @@ sub makeIndexArrayfromJumanResult{
 
 ## KNPの解析結果から索引語と索引付け対象となる係り受け関係を抽出する
 sub makeIndexfromKnpResult{
-    my($knp_result) = @_;
+    my($this,$knp_result) = @_;
 
     my %freq;
     foreach my $sent (split(/EOS\n/, $knp_result)){
-	my $pos = -1;
+	my $w_pos = -1;
+	my $bp_pos = -1;
 	my $kakariSaki = -1;
+	my $kakariType = undef;
 	my @bps = ();
 	foreach my $line (split(/\n/,$sent)){
 	    next if($line =~ /^\* \-?\d/);
 
-	    if($line =~ /^\+ (\-?\d+)/){
-		$pos++;
+ 	    if($line =~ /^\+ (\-?\d+)([a-zA-Z])/){
+		$bp_pos++;
 		$kakariSaki = $1;
-		$bps[$pos] = {kakarisaki => $kakariSaki,
-			      words => []
-			      };
+		$kakariType = $2;
+		$bps[$bp_pos] = {kakarisaki => $kakariSaki,
+				 kakaritype => $kakariType,
+				 words => [],
+				 w_poss => [],
+				 ab_w_poss => []
+		};
 	    }else{
 		next if ($line =~ /^(\<|\@|EOS)/);
 
 		my @m = split(/\s+/, $line);
+		$w_pos++;
+		$this->{absolute_pos}++;
 		## <意味有>タグが付与された形態素を索引付けする
 		if($line =~ /\<意味有\>/){
 		    next if ($line =~ /\<記号\>/); ## <意味有>タグがついてても<記号>タグがついていれば削除
@@ -289,50 +307,60 @@ sub makeIndexfromKnpResult{
 			    push(@reps, &toUpperCase_utf8($1));
 			}
 		    }
-		    push(@{$bps[$pos]->{words}}, \@reps);
+		    push(@{$bps[$bp_pos]->{words}},\@reps);
+		    push(@{$bps[$bp_pos]->{w_poss}}, $w_pos);
+		    push(@{$bps[$bp_pos]->{ab_w_poss}}, $this->{absolute_pos});
 		}
 	    } # end of else
 	} # end of foreach my $line (split(/\n/,$sent))
 
+	for(my $pos = scalar(@bps); $pos > 0; $pos--){
+	    print "# $bps[$pos-1]->{kakarisaki}\n";
+	    if($bps[$pos-1]->{kakaritype} eq 'P'){
+		my $kakariSaki = $bps[$pos-1]->{kakarisaki};
+		$bps[$pos-1]->{kakarisaki} = $bps[$kakariSaki]->{kakarisaki};
+		print "> $kakariSaki\n";
+	    }
+	}
+
 	## <意味有>が付いている形態素の代表表記を索引付け
 	## 代表表記が複数個ある場合は代表表記の個数で割ってカウントする
-	my $pos = 0;
-	for(my $i = 0; $i < scalar(@bps); $i++){
-	    next unless(defined($bps[$i]->{words}));
+	for(my $pos = 0; $pos < scalar(@bps); $pos++){
+	    next unless(defined($bps[$pos]->{words}));
 
-	    for(my $j = 0; $j < scalar(@{$bps[$i]->{words}}); $j++){
-		my $daihyou = $bps[$i]->{words}->[$j];
+	    for(my $i = 0; $i < scalar(@{$bps[$pos]->{words}}); $i++){
+		my $daihyou = $bps[$pos]->{words}->[$i];
 		my $num_daihyou = scalar(@{$daihyou});
-		for(my $k = 0; $k < $num_daihyou; $k++){
-		    $freq{"$daihyou->[$k]"}->{freq} += (1 / $num_daihyou);
-		    $freq{"$daihyou->[$k]"}->{pos} = $pos;
-		    $pos++;
+		for(my $j = 0; $j < $num_daihyou; $j++){
+		    $freq{"$daihyou->[$j]"}->{freq} += (1 / $num_daihyou);
+		    $freq{"$daihyou->[$j]"}->{pos} = $bps[$pos]->{w_poss}->[$i];
+		    $freq{"$daihyou->[$j]"}->{absolute_pos} = $bps[$pos]->{ab_w_poss}->[$i];
 		}
 	    }
 	}
 
 	## <意味有>が付いている形態素間の係り受け関係を索引付け
 	## 係り先・係り元に代表表記が複数個ある場合は、係り先・元の代表表記の個数の積で割ってカウントする
-	for(my $i = 0; $i < scalar(@bps); $i++){
-	    my $kakariSaki = $bps[$i]->{kakarisaki};
-	    ## 基本句が文末なら
+	for(my $pos = 0; $pos < scalar(@bps); $pos++){
+	    my $kakariSaki = $bps[$pos]->{kakarisaki};
+	    ## 基本句($bps[$pos])が文末(-1)なら
 	    if($kakariSaki < 0){
 		## <意味有>タグが付いている形態素が1つ
-		if(scalar($bps[$i]->{words}) < 2){
+		if(scalar($bps[$pos]->{words}) < 2){
 		    next;
 		}else{
-		    for(my $j = 0; $j < scalar(@{$bps[$i]->{words}}); $j++){
-			my $daihyou = $bps[$i]->{words}->[$j];
+		    for(my $i = 0; $i < scalar(@{$bps[$pos]->{words}}); $i++){
+			my $daihyou = $bps[$pos]->{words}->[$i];
 			my $num_daihyou_moto = scalar(@{$daihyou});
-			for(my $k = 0; $k < $num_daihyou_moto; $k++){
+			for(my $j = 0; $j < $num_daihyou_moto; $j++){
 			    my $kakariSakiDaihyou;
 			    ## 基本句に複数の<意味有>タグ付きの形態素がある場合は分解する
 			    ## ex)
 			    ## 河野洋平
 			    ## 河野->洋平
-			    if($j + 1 < scalar(@{$bps[$i]->{words}})){
+			    if($i + 1 < scalar(@{$bps[$pos]->{words}})){
 				## 隣りの形態素に係る
-				$kakariSakiDaihyou = $bps[$i]->{words}->[$j+1];
+				$kakariSakiDaihyou = $bps[$pos]->{words}->[$i+1];
 			    }else{
 				## 末尾の基本句なので終了(係り先なし)
 				next;
@@ -340,10 +368,10 @@ sub makeIndexfromKnpResult{
 			    next unless(defined($kakariSakiDaihyou)); ## 係り先基本句に<意味有>タグが付いた形態素が無ければ
 
 			    my $num_daihyou_saki = scalar(@{$kakariSakiDaihyou});
-			    for(my $l = 0; $l < $num_daihyou_saki; $l++){
-				$freq{$daihyou->[$k] . "->" . $kakariSakiDaihyou->[$l]}->{freq} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
-				$freq{$daihyou->[$k] . "->" . $kakariSakiDaihyou->[$l]}->{pos} = $pos;
-				$pos++;
+			    for(my $k = 0; $k < $num_daihyou_saki; $k++){
+				$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{freq} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
+				$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{pos} = $freq{$daihyou->[$j]}->{pos};
+				$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{absolute_pos} = $freq{$daihyou->[$j]}->{absolute_pos};
 			    }
 			}
 		    }
@@ -351,23 +379,23 @@ sub makeIndexfromKnpResult{
 		next;
 	    }
 
-	    next unless(defined($bps[$i]->{words})); ## <意味有>タグが付いている形態素が基本句に無いなら
+	    next unless(defined($bps[$pos]->{words})); ## <意味有>タグが付いている形態素が基本句に無いなら
 
-	    ## $i番目の基本句に含まれる形態素と、その係り先の基本句(曖昧性がある場合は全て)
+	    ## $pos番目の基本句に含まれる形態素と、その係り先の基本句(曖昧性がある場合は全て)
 	    ## との組を索引付け
-	    for(my $j = 0; $j < scalar(@{$bps[$i]->{words}}); $j++){
-		my $daihyou = $bps[$i]->{words}->[$j];
+	    for(my $i = 0; $i < scalar(@{$bps[$pos]->{words}}); $i++){
+		my $daihyou = $bps[$pos]->{words}->[$i];
 		my $num_daihyou_moto = scalar(@{$daihyou});
 
-		for(my $k = 0; $k < $num_daihyou_moto; $k++){
+		for(my $j = 0; $j < $num_daihyou_moto; $j++){
 		    my $kakariSakiDaihyou;
 		    ## 基本句に複数の<意味有>タグ付きの形態素がある場合は分解する
 		    ## ex)
 		    ## 河野洋平と/行った。
 		    ## 河野->洋平 洋平->行く
-		    if($j + 1 < scalar(@{$bps[$i]->{words}})){
+		    if($i + 1 < scalar(@{$bps[$pos]->{words}})){
 			## 隣りの形態素に係る
-			$kakariSakiDaihyou = $bps[$i]->{words}->[$j+1];
+			$kakariSakiDaihyou = $bps[$pos]->{words}->[$i+1];
 		    }else{
 			## 基本句全体で係っていた基本句に係る
 			$kakariSakiDaihyou = $bps[$kakariSaki]->{words}->[0];
@@ -376,10 +404,10 @@ sub makeIndexfromKnpResult{
 		    next unless(defined($kakariSakiDaihyou)); ## 係り先基本句に<意味有>タグが付いた形態素が無ければ
 
 		    my $num_daihyou_saki = scalar(@{$kakariSakiDaihyou});
-		    for(my $l = 0; $l < $num_daihyou_saki; $l++){
-			$freq{$daihyou->[$k] . "->" . $kakariSakiDaihyou->[$l]}->{freq} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
-			$freq{$daihyou->[$k] . "->" . $kakariSakiDaihyou->[$l]}->{pos} = $pos;
-			$pos++;
+		    for(my $k = 0; $k < $num_daihyou_saki; $k++){
+			$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{freq} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
+			$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{pos} = $freq{$daihyou->[$j]}->{pos};
+			$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{absolute_pos} = $freq{$daihyou->[$j]}->{absolute_pos};
 		    }
 		}
 	    }
@@ -389,7 +417,7 @@ sub makeIndexfromKnpResult{
 }
 
 sub makeIndexArrayfromKnpResult{
-    my($knp_result) = @_;
+    my($this,$knp_result) = @_;
 
     my $pos = -1;
     my @index_array;
@@ -440,7 +468,7 @@ sub makeIndexArrayfromKnpResult{
 }
 
 sub makeNgramIndex{
-    my($string, $N) = @_;
+    my($this, $string, $N) = @_;
     ## make search queries.
     my $size = length($string);
     my %indexes = ();
@@ -462,7 +490,7 @@ sub makeNgramIndex{
 }
 
 sub makeIndexfromIndexArray{
-    my($index_array, $window) = @_;
+    my($this, $index_array, $window) = @_;
     my %index = ();
     for(my $i = 0; $i < scalar(@{$index_array}); $i++){
 	foreach my $index_obj (@{$index_array->[$i]}){
