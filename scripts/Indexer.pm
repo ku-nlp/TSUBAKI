a00df6de27410d30870e29225b99385f7898305b
Keiji Shinzato <skeiji@(null)>
2007-03-28 14:52:15 +0000

    索引語の文中での位置、文書中での位置を保持するように変更.

--- scripts/Indexer.pm
+++ scripts/Indexer.pm
@@ -6,6 +6,7 @@ package Indexer;
 
 use strict;
 use utf8;
+use Encode;
 
 our @EXPORT = qw(makeIndexfromJumanResult makeIndexfromKnpResult makeNgramIndex makeIndexfromIndexArray);
 
@@ -119,151 +120,14 @@ sub makeIndexArrayfromJumanResult{
 }
 
 ## KNPの解析結果から索引語と索引付け対象となる係り受け関係を抽出する
-# sub makeIndexfromKnpResult{
-#     my($knp_result) = @_;
-
-#     my %freq;
-#     foreach my $sent (split(/EOS\n/, $knp_result)){
-# 	my $pos = -1;
-# 	my $kakariSaki = -1;
-# 	my @bps = ();
-# 	foreach my $line (split(/\n/,$sent)){
-# 	    next if($line =~ /^\* \-?\d/);
-
-# 	    if($line =~ /^\+ (\-?\d+)(P|D|d|A)/){
-# 		$pos++;
-# 		$kakariSaki = $1;
-# 		$bps[$pos] = {kakarisaki => $kakariSaki,
-# 			      words => [],
-# 			      type => $2
-# 			      };
-# 	    }else{
-# 		next if ($line =~ /^(\<|\@|EOS)/);
-
-# 		my @m = split(/\s+/, $line);
-# 		## <意味有>タグが付与された形態素を索引付けする
-# 		if($line =~ /\<意味有\>/){
-# 		    next if ($line =~ /\<記号\>/); ## <意味有>タグがついてても<記号>タグがついていれば削除
-# 		    next if (&containsSymbols($m[2]) > 0); ## <記号>タグがついてない記号を削除
-
-# 		    my $word = $m[2];
-# 		    my @reps = ();
-# 		    ## 代表表記の取得
-# 		    if($line =~ /代表表記:(.+?)\//){
-# 			$word = $1;
-# 		    }
-
-# 		    push(@reps, &toUpperCase_utf8($word));
-# 		    ## 代表表記に曖昧性がある場合は全部保持する
-# 		    while($line =~ /\<ALT(.+?)\>/){
-# 			$line = "$'";
-# 			if($1 =~ /代表表記:(.+?)\//){
-# 			    push(@reps, &toUpperCase_utf8($1));
-# 			}
-# 		    }
-# 		    push(@{$bps[$pos]->{words}}, \@reps);
-# 		}
-# 	    } # end of else
-# 	} # end of foreach my $line (split(/\n/,$sent))
-
-# 	## <意味有>が付いている形態素の代表表記を索引付け
-# 	## 代表表記が複数個ある場合は代表表記の個数で割ってカウントする
-# 	for(my $i = 0; $i < scalar(@bps); $i++){
-# 	    next unless(defined($bps[$i]->{words}));
-
-# 	    for(my $j = 0; $j < scalar(@{$bps[$i]->{words}}); $j++){
-# 		my $daihyou = $bps[$i]->{words}->[$j];
-# 		my $num_daihyou = scalar(@{$daihyou});
-# 		for(my $k = 0; $k < $num_daihyou; $k++){
-# 		    $freq{"$daihyou->[$k]"} += (1/$num_daihyou);
-# 		}
-# 	    }
-# 	}
-
-# 	## <意味有>が付いている形態素間の係り受け関係を索引付け
-# 	## 係り先・係り元に代表表記が複数個ある場合は、係り先・元の代表表記の個数の積で割ってカウントする
-# 	for(my $i = 0; $i < scalar(@bps); $i++){
-# 	    my $kakariSaki = $bps[$i]->{kakarisaki};
-# 	    my $kakariType = $bps[$i]->{type};
-# 	    next if($kakariType eq 'P');
-
-# 	    ## 基本句が文末なら
-# 	    if($kakariSaki < 0){
-# 		## <意味有>タグが付いている形態素が1つ
-# 		if(scalar($bps[$i]->{words}) < 2){
-# 		    next;
-# 		}else{
-# 		    for(my $j = 0; $j < scalar(@{$bps[$i]->{words}}); $j++){
-# 			my $daihyou = $bps[$i]->{words}->[$j];
-# 			my $num_daihyou_moto = scalar(@{$daihyou});
-# 			for(my $k = 0; $k < $num_daihyou_moto; $k++){
-# 			    my $kakariSakiDaihyou;
-# 			    ## 基本句に複数の<意味有>タグ付きの形態素がある場合は分解する
-# 			    ## ex)
-# 			    ## 河野洋平
-# 			    ## 河野->洋平
-# 			    if($j + 1 < scalar(@{$bps[$i]->{words}})){
-# 				## 隣りの形態素に係る
-# 				$kakariSakiDaihyou = $bps[$i]->{words}->[$j+1];
-# 			    }else{
-# 				## 末尾の基本句なので終了(係り先なし)
-# 				next;
-# 			    }
-# 			    next unless(defined($kakariSakiDaihyou)); ## 係り先基本句に<意味有>タグが付いた形態素が無ければ
-
-# 			    my $num_daihyou_saki = scalar(@{$kakariSakiDaihyou});
-# 			    for(my $l = 0; $l < $num_daihyou_saki; $l++){
-# 				$freq{$daihyou->[$k] . "->" . $kakariSakiDaihyou->[$l]} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
-# 			    }
-# 			}
-# 		    }
-# 		}
-# 		next;
-# 	    }
-
-# 	    next unless(defined($bps[$i]->{words})); ## <意味有>タグが付いている形態素が基本句に無いなら
-
-# 	    ## $i番目の基本句に含まれる形態素と、その係り先の基本句(曖昧性がある場合は全て)
-# 	    ## との組を索引付け
-# 	    for(my $j = 0; $j < scalar(@{$bps[$i]->{words}}); $j++){
-# 		my $daihyou = $bps[$i]->{words}->[$j];
-# 		my $num_daihyou_moto = scalar(@{$daihyou});
-
-# 		for(my $k = 0; $k < $num_daihyou_moto; $k++){
-# 		    my $kakariSakiDaihyou;
-# 		    ## 基本句に複数の<意味有>タグ付きの形態素がある場合は分解する
-# 		    ## ex)
-# 		    ## 河野洋平と/行った。
-# 		    ## 河野->洋平 洋平->行く
-# 		    if($j + 1 < scalar(@{$bps[$i]->{words}})){
-# 			## 隣りの形態素に係る
-# 			$kakariSakiDaihyou = $bps[$i]->{words}->[$j+1];
-# 		    }else{
-# 			## 基本句全体で係っていた基本句に係る
-# 			$kakariSakiDaihyou = $bps[$kakariSaki]->{words}->[0];
-# 		    }
-
-# 		    next unless(defined($kakariSakiDaihyou)); ## 係り先基本句に<意味有>タグが付いた形態素が無ければ
-
-# 		    my $num_daihyou_saki = scalar(@{$kakariSakiDaihyou});
-# 		    for(my $l = 0; $l < $num_daihyou_saki; $l++){
-# 			$freq{$daihyou->[$k] . "->" . $kakariSakiDaihyou->[$l]} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
-# 		    }
-# 		}
-# 	    }
-# 	}
-#     }
-#     return \%freq;
-# }
-
-## KNPの解析結果から索引語と索引付け対象となる係り受け関係を抽出する
 sub makeIndexfromKnpResult{
     my($this,$knp_result,$option) = @_;
 
     my %freq;
     foreach my $sent (split(/EOS\n/, $knp_result)){
-	my $w_pos = -1;
-	my $bp_pos = -1;
+	$this->{absolute_pos}++;
+
+	my $local_pos = -1;
 	my $kakariSaki = -1;
 	my $kakariType = undef;
 	my @bps = ();
@@ -271,25 +135,24 @@ sub makeIndexfromKnpResult{
 	    next if($line =~ /^\* \-?\d/);
 
  	    if($line =~ /^\+ (\-?\d+)([a-zA-Z])/){
-		$bp_pos++;
 		$kakariSaki = $1;
 		$kakariType = $2;
-		$bps[$bp_pos] = {kakarisaki => $kakariSaki,
-				 kakaritype => $kakariType,
-				 words => [],
-				 w_poss => [],
-				 ab_w_poss => []
-		};
+		push(@bps, {kakarisaki => $kakariSaki,
+			    kakaritype => $kakariType,
+			    words => []
+		     });
 	    }else{
 		next if ($line =~ /^(\<|\@|EOS)/);
 
 		my @m = split(/\s+/, $line);
-		$w_pos++;
-		$this->{absolute_pos}++;
+
 		## <意味有>タグが付与された形態素を索引付けする
 		if($line =~ /\<意味有\>/){
 		    next if ($line =~ /\<記号\>/); ## <意味有>タグがついてても<記号>タグがついていれば削除
 		    next if (&containsSymbols($m[2]) > 0); ## <記号>タグがついてない記号を削除
+
+		    $local_pos++;
+		    $this->{absolute_pos}++;
 
 		    my $word = $m[2];
 		    my @reps = ();
@@ -307,9 +170,12 @@ sub makeIndexfromKnpResult{
 			    push(@reps, &toUpperCase_utf8($1));
 			}
 		    }
-		    push(@{$bps[$bp_pos]->{words}},\@reps);
-		    push(@{$bps[$bp_pos]->{w_poss}}, $w_pos);
-		    push(@{$bps[$bp_pos]->{ab_w_poss}}, $this->{absolute_pos});
+
+		    my $word = {reps => \@reps,
+				local_pos => $local_pos,
+#				string => $line,
+				global_pos => $this->{absolute_pos}};
+		    push(@{$bps[-1]->{words}}, $word);
 		}
 	    } # end of else
 	} # end of foreach my $line (split(/\n/,$sent))
@@ -318,12 +184,10 @@ sub makeIndexfromKnpResult{
 	# スプーンとフォークで食べる
 	# スプーン->食べる、フォーク->食べる
 	for(my $pos = scalar(@bps); $pos > 0; $pos--){
-#	    print "# $bps[$pos-1]->{kakarisaki}\n";
 	    if($bps[$pos-1]->{kakaritype} eq 'P'){
 		my $kakariSaki = $bps[$pos-1]->{kakarisaki};
 		if($bps[$kakariSaki]->{kakarisaki} ne '-1'){
 		    $bps[$pos-1]->{kakarisaki} = $bps[$kakariSaki]->{kakarisaki};
-#		    print "> $bps[$pos-1]->{kakarisaki}\n";
 		}
 	    }
 	}
@@ -334,40 +198,45 @@ sub makeIndexfromKnpResult{
 	    next unless(defined($bps[$pos]->{words}));
 
 	    for(my $i = 0; $i < scalar(@{$bps[$pos]->{words}}); $i++){
-		my $daihyou = $bps[$pos]->{words}->[$i];
-		my $num_daihyou = scalar(@{$daihyou});
-		for(my $j = 0; $j < $num_daihyou; $j++){
-		    $freq{"$daihyou->[$j]"}->{freq} += (1 / $num_daihyou);
-		    $freq{"$daihyou->[$j]"}->{pos} = $bps[$pos]->{w_poss}->[$i];
-		    $freq{"$daihyou->[$j]"}->{absolute_pos} = $bps[$pos]->{ab_w_poss}->[$i];
+		my $word = $bps[$pos]->{words}->[$i];
+		my $reps = $word->{reps};
+		my $size = scalar(@{$reps});
+		for(my $j = 0; $j < $size; $j++){
+		    $freq{"$reps->[$j]"}->{freq} += (1 / $size);
+		    $freq{"$reps->[$j]"}->{pos} = $word->{local_pos};
+		    $freq{"$reps->[$j]"}->{absolute_pos} = $word->{global_pos};
 		}
 	    }
 	}
 
+	## KNPの解析結果から単語インデックスのみを抽出したい場合
 	return \%freq if $option->{no_dpnd};
 
 	## <意味有>が付いている形態素間の係り受け関係を索引付け
 	## 係り先・係り元に代表表記が複数個ある場合は、係り先・元の代表表記の個数の積で割ってカウントする
 	for(my $pos = 0; $pos < scalar(@bps); $pos++){
 	    my $kakariSaki = $bps[$pos]->{kakarisaki};
+	    my $kakariMoto = $bps[$pos]->{words};
 	    ## 基本句($bps[$pos])が文末(-1)なら
 	    if($kakariSaki < 0){
 		## <意味有>タグが付いている形態素が1つ
-		if(scalar($bps[$pos]->{words}) < 2){
+		if(scalar(@{$bps[$pos]->{words}}) < 2){
 		    next;
 		}else{
+		    ## 基本句に複数の<意味有>タグ付きの形態素がある場合は分解する
+		    ## ex)
+		    ## 河野洋平
+		    ## 河野->洋平
+
 		    for(my $i = 0; $i < scalar(@{$bps[$pos]->{words}}); $i++){
-			my $daihyou = $bps[$pos]->{words}->[$i];
-			my $num_daihyou_moto = scalar(@{$daihyou});
+			my $word = $bps[$pos]->{words}->[$i];
+			my $reps = $word->{reps};
+			my $num_daihyou_moto = scalar(@{$reps});
 			for(my $j = 0; $j < $num_daihyou_moto; $j++){
 			    my $kakariSakiDaihyou;
-			    ## 基本句に複数の<意味有>タグ付きの形態素がある場合は分解する
-			    ## ex)
-			    ## 河野洋平
-			    ## 河野->洋平
 			    if($i + 1 < scalar(@{$bps[$pos]->{words}})){
 				## 隣りの形態素に係る
-				$kakariSakiDaihyou = $bps[$pos]->{words}->[$i+1];
+				$kakariSakiDaihyou = $bps[$pos]->{words}->[$i + 1]->{reps};
 			    }else{
 				## 末尾の基本句なので終了(係り先なし)
 				next;
@@ -376,9 +245,9 @@ sub makeIndexfromKnpResult{
 
 			    my $num_daihyou_saki = scalar(@{$kakariSakiDaihyou});
 			    for(my $k = 0; $k < $num_daihyou_saki; $k++){
-				$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{freq} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
-				$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{pos} = $freq{$daihyou->[$j]}->{pos};
-				$freq{$daihyou->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{absolute_pos} = $freq{$daihyou->[$j]}->{absolute_pos};
+				$freq{$reps->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{freq} += (1 / ($num_daihyou_saki * $num_daihyou_moto));
+				$freq{$reps->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{pos} = $freq{$reps->[$j]}->{pos};
+				$freq{$reps->[$j] . "->" . $kakariSakiDaihyou->[$k]}->{absolute_pos} = $freq{$reps->[$j]}->{absolute_pos};
 			    }
 			}
 		    }
@@ -386,12 +255,13 @@ sub makeIndexfromKnpResult{
 		next;
 	    }
 
-	    next unless(defined($bps[$pos]->{words})); ## <意味有>タグが付いている形態素が基本句に無いなら
+	    next unless(defined($bps[$pos]->{words}->[0]->{reps})); ## <意味有>タグが付いている形態素が基本句に無いなら
 
 	    ## $pos番目の基本句に含まれる形態素と、その係り先の基本句(曖昧性がある場合は全て)
-	    ## との組を索引付け
+	    ## との組を索引語として抽出
 	    for(my $i = 0; $i < scalar(@{$bps[$pos]->{words}}); $i++){
-		my $daihyou = $bps[$pos]->{words}->[$i];
+		my $daihyou = $bps[$pos]->{words}->[$i]->{reps};
+#		print STDERR encode('euc-jp', ($bps[$pos]->{words}->[$i]->{string})) . "\n";
 		my $num_daihyou_moto = scalar(@{$daihyou});
 
 		for(my $j = 0; $j < $num_daihyou_moto; $j++){
@@ -402,10 +272,10 @@ sub makeIndexfromKnpResult{
 		    ## 河野->洋平 洋平->行く
 		    if($i + 1 < scalar(@{$bps[$pos]->{words}})){
 			## 隣りの形態素に係る
-			$kakariSakiDaihyou = $bps[$pos]->{words}->[$i+1];
+			$kakariSakiDaihyou = $bps[$pos]->{words}->[$i + 1]->{reps};
 		    }else{
 			## 基本句全体で係っていた基本句に係る
-			$kakariSakiDaihyou = $bps[$kakariSaki]->{words}->[0];
+			$kakariSakiDaihyou = $bps[$kakariSaki]->{words}->[0]->{reps};
 		    }
 
 		    next unless(defined($kakariSakiDaihyou)); ## 係り先基本句に<意味有>タグが付いた形態素が無ければ
