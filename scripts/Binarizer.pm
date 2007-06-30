3866b9460b9f10547be74877cda33d566279e4cc
Keiji Shinzato <skeiji@(null)>
2007-06-30 09:22:24 +0000

    頻度の小数点以下が無視されるバグを修正. 位置情報数をインデックスに書き込むよう変更. 位置情報をインデックスに含めるかどうかを指定するオプション -position の追加. インデックスに追加した情報を出力するオプション -verbose を追加.

--- scripts/Binarizer.pm
+++ scripts/Binarizer.pm
@@ -10,7 +10,7 @@ use Encode;
 use FileHandle;
 
 sub new {
-    my($class, $th, $datfp, $cdbfp) = @_;
+    my($class, $th, $datfp, $cdbfp, $position, $verbose) = @_;
     my $dat = new FileHandle;
     open($dat, "> $datfp") || die "$!\n";
     my $offset_cdb = new CDB_File ("$cdbfp", "$cdbfp.$$") or die;
@@ -22,7 +22,10 @@ sub new {
 		datfp => $datfp,
 		threshold => $th,
 		totalbytes => 0,
-		odbcnt => 0};
+		odbcnt => 0,
+		position => $position,
+		verbose => $verbose,
+    };
 
     bless $this;
 }
@@ -31,13 +34,10 @@ sub close {
     my ($this) = @_;
     $this->{odb}->finish();
     $this->{dat}->close();
-#   print STDERR "$this->{totalbytes}\n";
 }
 
 sub DESTROY {
     my($this) = @_;
-#   $this->{odb}->finish();
-#   $this->{dat}->close();
 }
 
 sub add {
@@ -49,25 +49,53 @@ sub add {
     # UTFエンコードされた単語を出力
     my $index_utf8 = encode('utf8', $index);
     print {$this->{dat}} $index_utf8;
+    print $index_utf8 if ($this->{verbose});
 
     # 0を出力
     print {$this->{dat}} pack('c', 0);
-#   print {$this->{dat}} ':0:';
+    print ':0:' if ($this->{verbose});
 
     # 文書数をlong型で出力
     print {$this->{dat}} pack('L', $dlength);
-#   print {$this->{dat}} "$dlength:";
+    print "$dlength:" if ($this->{verbose});
 
+    my $byte_pos = 0;
     for(my $i = 0; $i < $dlength; $i++) {
- 	my ($did, $freq) = split (/:/, $docs->[$i]);
+ 	my ($did, $freq_pos) = split (/:/, $docs->[$i]);
 
- 	# 単語IDと頻度をLONGで出力
+	my ($freq, $pos_str, @poss, $size);
+	if ($freq_pos =~ /@/) {
+	    ($freq, $pos_str) = split (/@/, $freq_pos);
+	    @poss = split (/,/, $pos_str);
+	    $size = scalar(@poss);
+	} else {
+	    if ($this->{position}) {
+		# position が指定されているのにインデックスの位置情報がない場合
+		print STDERR "\nOption error!\n";
+		print STDERR "Not found the locations of indexed terms.\n";
+		exit(1);
+	    }
+
+	    $freq = $freq_pos;
+	}
+
+ 	# 文書IDと頻度をLONGで出力
 	print {$this->{dat}} pack('L', $did);
-	print {$this->{dat}} pack('L', $freq);
-# 	print {$this->{dat}} "$did:";
-# 	print {$this->{dat}} "$freq:";
+	print {$this->{dat}} pack('L', ($freq * 10000) + 0.5); # 四捨五入
+ 	print "$did:" . int(($freq * 10000) + 0.5) . " " if ($this->{verbose});
+
+	if ($this->{position}) {
+	    print {$this->{dat}} pack('L', $size);
+	    print ":$size:" if ($this->{verbose});
+
+	    foreach my $pos (@poss) {
+		print {$this->{dat}} pack('L', $pos);
+		print "$pos," if ($this->{verbose});
+	    }
+	    $byte_pos += ($size * 4);
+	}
     }
-#   print {$this->{dat}} "\n";
+    print "\n" if ($this->{verbose});
 
     # オフセットを保存し、下の処理で書き込むバイト数分増やす
     $this->{totalbytes} += (length($index_utf8) + length("$this->{offset}"));
@@ -79,7 +107,18 @@ sub add {
     }
 
     $this->{odb}->insert($index, $this->{offset});
-    $this->{offset} += (length($index_utf8) + 5 + 8 * $dlength);
+
+    $this->{offset} += length($index_utf8); # 索引語
+    $this->{offset} += 1; # デリミタ0
+    $this->{offset} += 4; # 文書頻度
+    $this->{offset} += (2 * 4 * $dlength); # (文書ID + 出現頻度) * 文書数
+
+    if ($this->{position}) {
+	$this->{offset} += (4 * $dlength); # 位置情報数 * 文書数
+	$this->{offset} += $byte_pos; # 位置情報のサイズ
+    }
+
+#   printf ("offset: %s %d (next %d + %d + 4 + %d + %d)\n", $index_utf8, $this->{offset}, length($index_utf8), length('0'), (8 * $dlength), $byte_pos) if ($this->{verbose});
 }
 
 1;
