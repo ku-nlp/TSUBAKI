4501f769ef5f3d380d19439eaed91ccf2f28ee22
Keiji Shinzato <skeiji@(null)>
2007-07-19 01:53:20 +0000

    tf, df, lengthの値が0以下の場合は-1を返すように変更. 計算したスコアを表示するdebugオプションを追加.

--- scripts/OKAPI.pm
+++ scripts/OKAPI.pm
@@ -4,11 +4,12 @@ use strict;
 use utf8;
 
 sub new {
-    my ($class, $average_doc_length, $N) = @_;
+    my ($class, $average_doc_length, $N, $debug) = @_;
     my $this = {
 	method_name => 'OKAPI_BM25',
 	average_doc_length => $average_doc_length,
-	total_number_of_docs => $N
+	total_number_of_docs => $N,
+	debug => $debug
     };
 
     bless $this;
@@ -23,7 +24,10 @@ sub calculate_score {
     my $gdf = $args->{df};
     my $length = $args->{length};
 
+    return -1 if ($freq <= 0 || $gdf <= 0 || $length <= 0);
+
     my $tf = (3 * $freq) / ((0.5 + 1.5 * $length / $this->{average_doc_length}) + $freq);
+    print "log(($this->{total_number_of_docs} - $gdf + 0.5) / ($gdf + 0.5))\n" if ($this->{debug});
     my $idf =  log(($this->{total_number_of_docs} - $gdf + 0.5) / ($gdf + 0.5));
 
     return $tf * $idf;
