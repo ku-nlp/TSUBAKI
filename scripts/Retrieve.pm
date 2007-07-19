0523abb0cb31e7bc36da1fa8bd9c0c44cdffb93d
Keiji Shinzato <skeiji@(null)>
2007-07-19 02:33:21 +0000

    インデックスをコンストラクタで読み込むように変更. 文情報を持ったインデックスに対応.

--- scripts/Retrieve.pm
+++ scripts/Retrieve.pm
@@ -13,8 +13,8 @@ use utf8;
 use FileHandle;
 use Storable;
 use Error qw(:try);
-# use Devel::Size qw/size total_size/;
-# use Devel::Size::Report qw/report_size/;
+use Devel::Size qw/size total_size/;
+use Devel::Size::Report qw/report_size/;
 
 my $DEBUG = 1;
 my $host = `hostname`; chop($host);
@@ -49,6 +49,14 @@ sub new {
 	    tie %{$this->{OFFSET}[$fcnt]}, 'CDB_File', $offset_fp or die "$0: can't tie to $offset_fp $!\n";
 	    print STDERR "$host> done.\n" if ($this->{VERBOSE});
 	}
+
+	# idx*.datというファイルを読み込む
+	next if $d !~ /idx(\d+).$this->{TYPE}.dat$/;
+	my $id = $1;
+
+	# ファイル(idx*.dat)をオープンする
+	$this->{IN}[$fcnt] = new FileHandle;
+	open($this->{IN}[$fcnt], "< $dir/idx$id.$this->{TYPE}.dat") or die "$dir/idx$id.$this->{TYPE}.dat: $!\n";
 	$fcnt++;
     }
     closedir(DIR);
@@ -71,27 +79,10 @@ sub printLog {
 }
 
 sub search {
-    my($this, $keyword, $already_retrieved_docs, $add_flag, $no_position) = @_;
-
-    ## INDEXファイルの読み込み
-    my $fcnt = 0;
-    my $dir = $this->{INDEX_DIR};
-    opendir(DIR, $dir);
-    foreach my $d (sort readdir(DIR)) {
-	# idx*.datというファイルを読み込む
-	next if $d !~ /idx(\d+).$this->{TYPE}.dat$/;
-	my $id = $1;
-
-	# ファイル(idx*.dat)をオープンする
-	$this->{IN}[$fcnt] = new FileHandle;
-	open($this->{IN}[$fcnt], "< $dir/idx$id.$this->{TYPE}.dat") or die "$dir/idx$id.$this->{TYPE}.dat: $!\n";
-	$fcnt++;
-    }
-    closedir(DIR);
-
+    my($this, $keyword, $already_retrieved_docs, $add_flag, $no_position, $sentence_flag, $syngraph_search) = @_;
     my @docs = ();
     ## idxごとに検索
-    for (my $f_num = 0; $f_num < $fcnt; $f_num++) {
+    for (my $f_num = 0; $f_num < scalar(@{$this->{OFFSET}}); $f_num++) {
 	my $offset = $this->{OFFSET}[$f_num]->{$keyword->{string}};
 	# オフセットがあるかどうかのチェック
 	unless (defined($offset)) {
@@ -115,8 +106,8 @@ sub search {
 		
 		# 次にキーワードの文書頻度
 		read($this->{IN}[$f_num], $buf, 4);
-		
 		my $ldf = unpack('L', $buf);
+
 		print STDERR "$host> keyword=$keyword->{string},ldf=$ldf\n" if ($this->{VERBOSE});
 #		printf "ldf  = %d Byte.\n", (total_size($ldf));
 
@@ -124,50 +115,68 @@ sub search {
 		for (my $j = 0; $j < $ldf; $j++) {
 		    read($this->{IN}[$f_num], $buf, 4);
 		    my $did = (unpack('L', $buf) / 1);
-#		    my $did = 0;
-#		    printf "did :: $did  = %d Byte.\n", (total_size($did));
 
-		    read($this->{IN}[$f_num], $buf, 4);
-#		    my $freq = unpack('L', $buf);
- 		    my $freq = unpack('f', $buf);
-#		    printf "freq = %d Byte.\n", (total_size($freq));
-		    
-		    my @pos = ();
-		    unless ($this->{SKIPPOS}) {
+		    if (!exists($already_retrieved_docs->{$did}) && $add_flag < 1) {
+			seek($this->{IN}[$f_num], 4, 1); # 出現頻度をスキップ
+			unless ($this->{SKIPPOS}) {
+			    read($this->{IN}[$f_num], $buf, 4);
+			    my $sid_size = unpack('L', $buf);
+			    seek($this->{IN}[$f_num], $sid_size * 4, 1); # 文情報をスキップ
+
+			    read($this->{IN}[$f_num], $buf, 4);
+			    my $pos_size = unpack('L', $buf);
+			    seek($this->{IN}[$f_num], $pos_size * 4, 1); # 位置情報をスキップ
+			}
+		    } else {
+			$already_retrieved_docs->{$did} = 1;
+
 			read($this->{IN}[$f_num], $buf, 4);
-			my $size = unpack('L', $buf);
-#			my $size = $freq;
 
-#			printf "$j docs %d Byte.\n", 0;
-			
-			if (!exists($already_retrieved_docs->{$did}) && $add_flag < 1) {
-			    seek($this->{IN}[$f_num], $size * 4, 1);
+			my $freq;
+			if ($syngraph_search < 0) {
+			    $freq = unpack('f', $buf);
 			} else {
-			    $already_retrieved_docs->{$did} = 1;
-			    
+			    $freq = unpack('L', $buf) / 10000;
+			}
+		    
+			my @sid = ();
+			my @pos = ();
+			unless ($this->{SKIPPOS}) {
+			    read($this->{IN}[$f_num], $buf, 4);
+			    my $sid_size = unpack('L', $buf);
 			    if ($no_position < 0) {
-				seek($this->{IN}[$f_num], $size * 4, 1);
+				seek($this->{IN}[$f_num], $sid_size * 4, 1);
 			    } else {
-				for (my $k = 0; $k < $size; $k++) {
+				for (my $k = 0; $k < $sid_size; $k++) {
+				    read($this->{IN}[$f_num], $buf, 4);
+				    my $s = unpack('L', $buf);
+				    push(@sid, $s);
+				}
+			    }
+
+			    read($this->{IN}[$f_num], $buf, 4);
+			    my $pos_size = unpack('L', $buf);
+			    if ($no_position < 0) {
+				seek($this->{IN}[$f_num], $pos_size * 4, 1);
+			    } else {
+				for (my $k = 0; $k < $pos_size; $k++) {
 				    read($this->{IN}[$f_num], $buf, 4);
 				    my $p = unpack('L', $buf);
 				    push(@pos, $p);
 				}
 			    }
 			}
+			
+			if ($no_position < 0) {
+			    push(@docs, [$did, $freq]);
+			} else {
+			    if ($sentence_flag > 0) {
+				push(@docs, [$did, $freq, \@sid]);
+			    } else {
+				push(@docs, [$did, $freq, \@pos]);
+			    }
+			}
 		    }
-
-		    if ($no_position < 0) {
-			push(@docs, [$did, $freq]);
-		    } else {
-			push(@docs, [$did, $freq, \@pos]);
-		    }
-
-#		    if ($j % 1113 == 0) {
-#			printf "$j $size docs %d Byte.\n", 0;
-#			printf "$j $keyword->{qid} docs %d Byte.\n", (total_size(\@docs));
-#			print "-----\n";
-#		    }
 		}
 		last;
 	    }
@@ -177,11 +186,19 @@ sub search {
 #    printf "$keyword->{qid} docs %.3f M Byte (size=%d).\n", (total_size(\@docs)) / (1024 * 1024), scalar(@docs);
 #    print "-----\n";
 
+#     foreach my $fh (@{$this->{IN}}) {
+# 	close($fh);
+#     }
+
     return \@docs;
 }   
 
 sub DESTROY {
     my ($this) = @_;
+
+    foreach my $fh (@{$this->{IN}}) {
+	close($fh);
+    }
 
     # ファイル(idx*.dat)をクローズ、OFFSET(offset*.dat)をuntieする
     for (my $f_num = 0; $f_num < scalar(@{$this->{OFFSET}}); $f_num++) {
