d5fc25298e75a1a0304965b3950b5ade699b2395
Keiji Shinzato <skeiji@(null)>
2007-12-05 18:22:52 +0000

    KNPとSynGraphでわかれていた重要文抽出関数を統合.

--- cgi/SnippetMaker.pm
+++ cgi/SnippetMaker.pm
@@ -6,6 +6,12 @@ use utf8;
 
 use Indexer;
 use Data::Dumper;
+{
+    package Data::Dumper;
+    sub qquote { return shift; }
+}
+$Data::Dumper::Useperl = 1;
+
 
 
 sub makeSnippetfromDocument {
@@ -163,15 +169,17 @@ sub extractSentencefromKnpResult {
 	    $buff = decode('utf8', $buff);
 	    if($buff =~ m/<Annotation Scheme=\"Knp\"><!\[CDATA\[((?:.|\n)+?)\]\]><\/Annotation>/){
 		my $knpresult = $1;
-
 		my $score = 0;
 		my $indice = $indexer->makeIndexfromKnpResult($knpresult);
 
-		foreach my $qk (@{$query}) {
+		my %matched_queries = ();
+		for (my $q = 0; $q < scalar(@{$query}); $q++) {
+		    my $qk = $query->[$q];
 		    foreach my $reps (@{$qk->{words}}) {
 			foreach my $rep (@{$reps}) {
 			    if (exists($indice->{$rep->{string}})) {
-				$score += $indice->{$rep->{string}}{freq};
+				$score += 1;# $indice->{$rep->{string}}{freq};
+				$matched_queries{$q}++;
 			    }
 			}
 		    }
@@ -179,7 +187,8 @@ sub extractSentencefromKnpResult {
 		    foreach my $reps (@{$qk->{dpnds}}) {
 			foreach my $rep (@{$reps}) {
 			    if (exists($indice->{$rep->{string}})) {
-				$score += $indice->{$rep->{string}}{freq};
+				$score += 2; # * $indice->{$rep->{string}}{freq};
+				$matched_queries{$q}++;
 			    }
 			}
 		    }
@@ -193,7 +202,8 @@ sub extractSentencefromKnpResult {
 			surfs => [],
 			reps => [],
 			sid => $sid,
-			score => $score
+			score => $score,
+			number_of_included_queries => scalar(keys %matched_queries)
 		    };
 
 		    my $word_list = &make_word_list($knpresult);
@@ -208,7 +218,7 @@ sub extractSentencefromKnpResult {
 		    }
 		    
 		    unless (exists($sbuff{$sentence->{rawstring}})) {
-			$sentence->{score} = $sentence->{score} * log(scalar(@{$sentence->{surfs}}));
+			$sentence->{score} = $sentence->{number_of_included_queries} * $sentence->{score} * log(scalar(@{$sentence->{surfs}}));
 			push(@sentences, $sentence);
 			$sbuff{$sentence->{rawstring}} = 1;
 		    }
@@ -223,12 +233,176 @@ sub extractSentencefromKnpResult {
     return \@sentences;
 }
 
+my $SF_DIR_PREFFIX1 = "/data/sfs";
+my $SF_DIR_PREFFIX2 = "/data/sfs_w_syn";
+my $SF_DIR_PREFFIX3 = "/net2/nlpcf34/disk09/skeiji/sfs_w_syn";
+my $SF_DIR_PREFFIX4 = "/net2/nlpcf34/disk03/skeiji/sfs_w_syn";
+
+sub extract_sentences_from_ID {
+    my($query, $id, $opt) = @_;
+
+    my $xmlfile;
+    if ($opt->{syngraph}) {
+	$xmlfile = sprintf("%s/x%03d/x%05d/%09d.xml.gz", $SF_DIR_PREFFIX2, $id / 1000000, $id / 10000, $id);
+	$xmlfile = sprintf("%s/x%05d/%09d.xml.gz", $SF_DIR_PREFFIX3, $id / 10000, $id) unless (-e $xmlfile);
+	$xmlfile = sprintf("%s/x%05d/%09d.xml.gz", $SF_DIR_PREFFIX4, $id / 10000, $id) unless (-e $xmlfile);
+	$xmlfile = sprintf("%s/x%03d/x%05d/%09d.xml.gz", $SF_DIR_PREFFIX1, $id / 1000000, $id / 10000, $id) unless (-e $xmlfile);
+    } else {
+	$xmlfile = sprintf("%s/x%03d/x%05d/%09d.xml.gz", $SF_DIR_PREFFIX1, $id / 1000000, $id / 10000, $id);
+	$xmlfile = sprintf("%s/x%03d/x%05d/%09d.xml.gz", $SF_DIR_PREFFIX2, $id / 1000000, $id / 10000, $id) unless (-e $xmlfile);
+    }
+
+    return &extract_sentences_from_standard_format($query, $xmlfile, $opt);
+}
+
+sub extract_sentences_from_standard_format {
+    my($query, $xmlfile, $opt) = @_;
+
+    open(READER,"zcat $xmlfile |") or die "$!";
+
+    if ($opt->{verbose}) {
+	print "extract from $xmlfile.\n";
+	print "option:\n";
+	print Dumper($opt) . "\n";
+    }
+
+    my $annotation;
+    my $indexer = new Indexer();
+    my @sentences = ();
+    my %sbuff = ();
+    my $sid = 0;
+    my $in_title = 0;
+    while (<READER>) {
+	if ($opt->{discard_title}) {
+	    # タイトルはスニッペッツの対象としない
+	    if ($_ =~ /<Title [^>]+>/) {
+		$in_title = 1;
+	    } elsif ($_ =~ /<\/Title>/ || $_ =~ /<\/Header>/) {
+		$in_title = 0;
+	    }
+	}
+	next if ($in_title > 0);
+	next if ($_ =~ /^!/ && !$opt->{syngraph});
+
+	$annotation .= $_;
+	if ($_ =~ m!</Annotation>!) {
+#	    print encode('euc-jp', decode('utf8', $annotation));
+
+	    $annotation = decode('utf8', $annotation);
+	    if ($annotation =~ m/<Annotation Scheme=\".+?\"><!\[CDATA\[((?:.|\n)+?)\]\]><\/Annotation>/) {
+		my $result = $1;
+		my $indice = ($opt->{syngraph}) ? $indexer->makeIndexfromSynGraph($result) : $indexer->makeIndexfromKnpResult($result);
+		my ($num_of_queries, $num_of_types) = &calculate_score($query, $indice, $opt);
+
+		my $sentence = {
+		    rawstring => undef,
+		    score => 0,
+		    smoothed_score => 0,
+		    words => {},
+		    dpnds => {},
+		    surfs => [],
+		    reps => [],
+		    sid => $sid,
+		    number_of_included_queries => $num_of_queries,
+		    number_of_included_query_types => $num_of_types
+		};
+
+		$sentence->{result} = $result if ($opt->{keep_result});
+
+		my $word_list = ($opt->{syngraph}) ?  &make_word_list_syngraph($result):  &make_word_list($result);
+
+		my $num_of_whitespace_cdot_comma = 0;
+		foreach my $w (@{$word_list}) {
+		    my $surf = $w->{surf};
+		    my $reps = $w->{reps};
+		    $num_of_whitespace_cdot_comma++ if ($surf =~ /　|・|，|、|＞|−|｜|／/);
+
+		    $sentence->{rawstring} .= $surf;
+		    push(@{$sentence->{surfs}}, $surf);
+		    push(@{$sentence->{reps}}, $w->{reps});
+		}
+
+		my $length = scalar(@{$sentence->{surfs}});
+		$length = 1 if ($length < 1);
+		my $score = $sentence->{number_of_included_query_types} * $sentence->{number_of_included_queries} * log($length);
+		$sentence->{score} = $score;
+		$sentence->{smoothed_score} = $score;
+		$sentence->{length} = $length;
+		$sentence->{num_of_whitespaces} = $num_of_whitespace_cdot_comma;
+
+		push(@sentences, $sentence);
+#		print encode('euc-jp', $sentence->{rawstring}) . " (" . $num_of_whitespace_cdot_comma . ") is SKIPPED.\n";
+	    }
+	    $sid++;
+	    $annotation = '';
+	}
+    }
+    close(READER);
+
+    my $window_size = $opt->{window_size};
+    my $size = scalar(@sentences);
+    for (my $i = 0; $i < scalar(@sentences); $i++) {
+	my $s = $sentences[$i];
+	for (my $j = 0; $j < $window_size; $j++) {
+	    my $k = $i - $j - 1;
+	    last if ($k < 0);
+	    $sentences[$k]->{smoothed_score} += ($s->{score} / (2 ** ($j + 1)));
+	}
+
+	for (my $j = 0; $j < $window_size; $j++) {
+	    my $k = $i + $j + 1;
+	    last if ($k > $size - 1);
+	    $sentences[$k]->{smoothed_score} += ($s->{score} / (2 ** ($j + 1)));
+	}
+    }
+
+    return \@sentences;
+}
+
+sub calculate_score {
+    my ($query, $indice, $opt) = @_;
+
+    my $num_of_queries = 0;
+    my %matched_queries = ();
+    my %buf;
+    foreach my $idx (@$indice) {
+	$buf{$idx->{midashi}}++;
+    }
+
+    for (my $q = 0; $q < scalar(@{$query}); $q++) {
+	my $qk = $query->[$q];
+	foreach my $reps (@{$qk->{words}}) {
+	    foreach my $rep (@{$reps}) {
+		my $k = $rep->{string};
+		if (exists($buf{$k})) {
+		    $num_of_queries += $buf{$k};
+		    $matched_queries{$k}++;
+		}
+	    }
+	}
+
+	foreach my $reps (@{$qk->{dpnds}}) {
+	    foreach my $rep (@{$reps}) {
+		my $k = $rep->{string};
+		if (exists($buf{$k})) {
+		    $num_of_queries += $buf{$k};
+		    $matched_queries{$k}++;
+		}
+	    }
+	}
+    }
+
+    return ($num_of_queries, scalar(keys %matched_queries));
+}
+
 sub make_word_list {
     my ($sent) = @_;
 
     my @words;
     foreach my $line (split(/\n/,$sent)){
-	next if($line =~ /^\* \-?\d/);
+	next if ($line =~ /^\* \-?\d/);
+	next if ($line =~ /^!/);
+	next if ($line =~ /^S\-ID/);
 
 	unless ($line =~ /^\+ (\-?\d+)([a-zA-Z])/){
 	    next if ($line =~ /^(\<|\@|EOS)/);
@@ -249,6 +423,7 @@ sub make_word_list {
 	    ## 代表表記に曖昧性がある場合は全部保持する
 	    ## ただし表記・読みが同一の代表表記は区別しない
 	    ## ex) 日本 にっぽん 日本 名詞 6 地名 4 * 0 * 0 "代表表記:日本/にほん" <代表表記:日本/にほん><品曖><ALT-日本-にほん-日本-6-4-0-0-"代表表記:日本/にほん"> ...
+	    my $lnbuf = $line;
 	    while ($line =~ /\<ALT(.+?)\>/) {
 		$line = "$'";
 		my $alt_cont = $1;
@@ -260,6 +435,7 @@ sub make_word_list {
 		    $reps{&toUpperCase_utf8($midashi)} = 1;
 		}
 	    }
+	    $line = $lnbuf;
 	    
 	    my @reps_array = sort keys %reps;
 	    my $word = {
@@ -314,15 +490,17 @@ sub make_word_list_syngraph {
 	    $wordcnt++;
 	} else {
 	    my ($dumy, $bnstId, $syn_node_str) = split(/ /, $line);
-	    if ($line =~ m!<SYNID:([^>]+)><スコア:((?:\d|\.)+)>(<[^>]+>)*$!) {
+	    if ($line =~ m!<SYNID:([^>]+)><スコア:((?:\d|\.)+)>((<[^>]+>)*)$!) {
 		my $sid = $1;
-		my $features = $3;
 		$sid = $1 if ($sid =~ m!^([^/]+)/!);
 		
+		my $features = $3;
+		$features = "$`$'" if ($features =~ /\<上位語\>/); # <上位語>を削除
+		$features =~ s/<下位語数:\d+>//; # <下位語数:(数字)>を削除
+
 		foreach my $bid (split(/,/, $bnstId)) {
 		    foreach my $pos (@{$bnst2pos{$bid}}) {
 			next if ($words[$pos]->{isContentWord} < 1);
-			
 			push(@{$words[$pos]->{reps}}, "$sid$features");
 		    }
 		}
