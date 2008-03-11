cae533e4f30247cd9616366bf3b33ca9c7632acc
Keiji Shinzato <skeiji@(null)>
2008-03-11 01:26:37 +0000

    added.

---
+++ scripts/make-url-title-dbs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# URL、タイトルデータベースを作成するスクリプト
+
+# ★以下の変数の値を変えること
+scriptdir=$HOME/cvs/SearchEngine/scripts
+
+
+indir=$1
+ofile=$indir.url.title
+
+rm $ofile 2> /dev/null
+
+# $indirは10000件分の標準フォーマットが収められたディレクトリがあるディレクトリ
+for d in `ls $indir`
+do
+    # $dは10000件分の標準フォーマットが収められたディレクトリ
+    echo "perl $scriptdir/extract-url-title.perl -z -url -title -dir $indir/$d >> $ofile"
+    perl $scriptdir/extract-url-title.perl -z -url -title -dir $indir/$d >> $ofile
+done
+
+echo perl $scriptdir/make-url-title-cdbs.perl $ofile
+perl $scriptdir/make-url-title-cdbs.perl $ofile
+
+mv $ofile.title.cdb $indir.title.cdb
+mv $ofile.url.cdb $indir.url.cdb
