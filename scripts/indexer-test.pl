587abfeaa9ae271793a701a8e50f03dacec7a5a7
Keiji Shinzato <skeiji@(null)>
2008-03-25 04:53:51 +0000

    Indexer.pmの変更に対応．

--- scripts/indexer-test.pl
+++ scripts/indexer-test.pl
@@ -4,6 +4,8 @@ use strict;
 use utf8;
 use Indexer;
 use Getopt::Long;
+use KNP;
+use KNP::Result;
 
 my (%opt);
 GetOptions(\%opt, 'syn');
@@ -16,9 +18,13 @@ while (<STDIN>) {
     $buf .= $_;
 }
 
-my $indexer = new Indexer();
-my $indice = ($opt{syn}) ? $indexer->makeIndexfromSynGraph($buf) : $indexer->makeIndexfromKnpResult($buf);
+my $indexer = new Indexer( {ignore_yomi => 1});
+# my $indice = ($opt{syn}) ? $indexer->makeIndexfromSynGraph($buf) : $indexer->makeIndexfromKnpResult($buf, {only_kihonkei => 1, with_kihonkei => 1, without_yomi => 1});
+# my $indice = ($opt{syn}) ? $indexer->makeIndexfromSynGraph($buf) : $indexer->make_index_from_KNP_result_object($buf, {only_kihonkei => 1, with_kihonkei => 1, without_yomi => 1});
+
+my $result = new KNP::Result($buf);
+my $indice = $indexer->makeIndexFromKNPResult($result, {only_kihonkei => 1, with_kihonkei => 1, without_yomi => 1});
 
 foreach my $e (@{$indice}) {
-    print $e->{midashi} . "\n" if ($e->{isContentWord});
+    printf("%s %.4f\n", $e->{rawstring}, $e->{freq}) if ($e->{isContentWord});
 }
