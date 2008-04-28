4b0d9c9fc7d2fb60771e6b5f464a461d3da47a49
Keiji Shinzato <skeiji@(null)>
2008-04-28 01:00:53 +0000

    標準フォーマット取得オプションの追加.

--- utils/api_req.pl
+++ utils/api_req.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+# TSUBAKI APIに問い合わせて、検索結果、標準フォーマットを取得するスクリプト
+
 # $Id$
 
 use LWP::UserAgent;
@@ -11,39 +13,81 @@ use Getopt::Long;
 use strict;
 
 my (%opt);
-GetOptions(\%opt, 'query=s', 'start=i', 'results=i');
+GetOptions(\%opt, 'query=s', 'start=i', 'results=i', 'download=s', 'proxy', 'proxy_server=s', 'verbose');
 
-# 検索したいキーワード (utf8)をURIエンコードする
-# my $encoding = guess_encoding($opt{query}, qw/ascii euc-jp shiftjis 7bit-jis utf8/)->name();
-my $encoding = 'euc-jp';
-Encode::from_to($opt{query}, $encoding, 'utf8');
-my $uri_escaped_query = uri_escape($opt{query});
+$opt{proxy_server} = 'http://proxy.kuins.net:8080' if ($opt{proxy} && !$opt{proxy_server});
 
-# リクエストURLを作成
-my $base_url = 'http://nlpc06.ixnlp.nii.ac.jp/api.cgi';
-my $results = (defined $opt{results}) ? $opt{results} : 10;
-my $start = (defined $opt{start}) ? $opt{start} : 1;
-my $req_url = "$base_url?query=$uri_escaped_query&results=$results&start=$start";
-$req_url .= "&logical_operator=AND&dpnd=1&force_dpnd=0&no_snippets=1&result_items=Id";
+&main();
 
-# UserAgent の作成
-my $ua = new LWP::UserAgent();
-# Proxy の設定
-$ua->proxy('http', 'http://proxy.kuins.net:8080');
-# タイムアウトの設定
-$ua->timeout(3600);
+sub main {
+    # 検索したいキーワード (utf8)をURIエンコードする
+    # my $encoding = guess_encoding($opt{query}, qw/ascii euc-jp shiftjis 7bit-jis utf8/)->name();
+    my $encoding = 'euc-jp';
+    Encode::from_to($opt{query}, $encoding, 'utf8');
+    my $uri_escaped_query = uri_escape($opt{query});
 
-# リクエストの送信
-my $req = HTTP::Request->new(GET => $req_url);
-$req->header('Accept' => 'text/xml');
-my $response = $ua->request($req);
+    # リクエストURLを作成
+    my $base_url = 'http://tsubaki.ixnlp.nii.ac.jp/api.cgi';
+    my $results = (defined $opt{results}) ? $opt{results} : 10;
+    my $start = (defined $opt{start}) ? $opt{start} : 1;
+    my $req_url = "$base_url?query=$uri_escaped_query&results=$results&start=$start";
+    $req_url .= "&logical_operator=AND&dpnd=1&force_dpnd=0&no_snippets=1&result_items=Id";
 
-# TSUBAKI APIの結果を取得
-if ($response->is_success()) {
-    my $xmldat = $response->content();
-    Encode::from_to($xmldat, 'utf8', $encoding);
-    $xmldat =~ s/<\?xml version="1.0" encoding="utf\-8"\?>/<\?xml version="1.0" encoding="$encoding"\?>/;
-    print $xmldat;
-} else {
-    print STDERR "Failed to call the TSUBAKI API.\n";
+    print STDERR $req_url . "\n" if ($opt{verbose});
+
+    # UserAgent の作成
+    my $ua = new LWP::UserAgent();
+    # Proxy の設定
+    $ua->proxy('http', $opt{proxy_server}) if ($opt{proxy});
+    # タイムアウトの設定
+    $ua->timeout(3600);
+
+    # リクエストの送信
+    my $req = HTTP::Request->new(GET => $req_url);
+    $req->header('Accept' => 'text/xml');
+    my $response = $ua->request($req);
+
+    # TSUBAKI APIの結果を取得
+    my $xmldat;
+    if ($response->is_success()) {
+	$xmldat = $response->content();
+
+	unless ($opt{download}) {
+	    print $xmldat;
+	} else {
+	    mkdir($opt{download}) unless (-e $opt{download});
+
+	    open(WRITER, sprintf("> %s/result.xml", $opt{download})) or die;
+	    print WRITER $xmldat;
+	    close(WRITER);
+	}
+
+	print STDERR "Succeeded in getting search reuslts.\n" if ($opt{verbose});
+    } else {
+	print STDERR "Failed to call the TSUBAKI API.\n";
+    }
+
+    if ($xmldat && $opt{download}) {
+	while ($xmldat =~ /<Result Rank="(\d+)" Id="(\d+)">/g) {
+	    my $rank = $1;
+	    my $did = $2;
+
+	    my $requestURL = "$base_url?id=$did&format=xml";
+
+	    # リクエストの送信
+	    my $req = HTTP::Request->new(GET => $requestURL);
+	    $req->header('Accept' => 'text/xml');
+	    my $response = $ua->request($req);
+
+	    if ($response->is_success()) {
+		open(WRITER, sprintf("> %s/%04d.xml", $opt{download}, $rank)) or die;
+		print WRITER $response->content();
+		close(WRITER);
+
+		print STDERR "Succeeded in getting the standard format file (Rank=$rank, Id=$did).\n" if ($opt{verbose});
+	    } else {
+		print STDERR "Failed to call the TSUBAKI API.\n";
+	    }
+	}
+    }
 }
