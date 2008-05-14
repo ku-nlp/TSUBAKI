1930141aea531c68508f166828a9deadf6b41da7
Keiji Shinzato <skeiji@(null)>
2008-05-14 13:57:16 +0000

    kwicオプションの追加.

--- scripts/snippet_maker_test.pl
+++ scripts/snippet_maker_test.pl
@@ -13,9 +13,10 @@ my $JUMAN_PATH = $TOOL_HOME;
 my $SYNDB_PATH = "$ENV{HOME}/cvs/SynGraph/syndb/i686";
 
 my (%opt);
-GetOptions(\%opt, 'query=s', 'z', 'syngraph', 'debug', 'encoding=s', 'discard_title', 'syndb=s', 'string_mode', 'window_size=s', 'debug');
+GetOptions(\%opt, 'query=s', 'z', 'syngraph', 'debug', 'encoding=s', 'discard_title', 'syndb=s', 'string_mode', 'window_size=s', 'debug', 'kwic', 'kwic_window_size=s');
 
-$SYNDB_PATH = $opt{syndb} if($opt{syndb});
+$SYNDB_PATH = $opt{syndb} if ($opt{syndb});
+$opt{kwic_window_size} = 5 unless ($opt{kwic_window_size});
 
 require SynGraph if ($opt{syngraph});
 $opt{encoding} = 'euc-jp' unless ($opt{encoding});
@@ -50,13 +51,29 @@ sub main {
 	my $sentences = &SnippetMaker::extract_sentences_from_standard_format($query->{keywords}, $file, \%opt);
 #	my $sentences = &SnippetMaker::extract_sentences_from_ID($query->{keywords}, $id, \%opt);
 	foreach my $s (@$sentences) {
+	    print $s->{including_all_indices} . " ";
 	    print $file . " ";
 	    print "sid " . $s->{sid} . " ";
 	    print "score " . $s->{score} . " ";
 	    print "smoothed " . $s->{smoothed_score} . " ";
 	    printf("whitespace %.2f ", $s->{num_of_whitespaces} / $s->{length});
 
-	    print $s->{rawstring} . "\n";
+	    if ($opt{kwic}) {
+		my $p = $s->{firstPositionOfHeadWords};
+		my $start = ($p - $opt{kwic_window_size} > 0) ? $p - $opt{kwic_window_size}: 0;
+		my $end = ($p + $opt{kwic_window_size} < scalar(@{$s->{surfs}})) ? $p + $opt{kwic_window_size} : scalar(@{$s->{surfs}}) - 1;
+		for (my $i = $start; $i < $end; $i++) {
+		    if ($i == $p) {
+			print " [" . $s->{surfs}[$i] . "] ";
+		    } else {
+			print $s->{surfs}[$i];
+		    }
+		}
+		print " " . $start . " $p " . $end if ($opt{debug});
+		print "\n";
+	    } else {
+		print $s->{rawstring} . "\n";
+	    }
 	}
     }
 }
