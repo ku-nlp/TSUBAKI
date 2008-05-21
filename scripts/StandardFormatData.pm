a72e58a9172e8c3f489e98b9dcffd0d56b194aa0
Keiji Shinzato <skeiji@(null)>
2008-05-21 01:23:09 +0000

    不要なコードの削除, 読み込み済みの標準フォーマットをコンストラクタで受け付けるようにした, parseLinkInfoメソッド内の正規表現のバグを修正.

--- scripts/StandardFormatData.pm
+++ scripts/StandardFormatData.pm
@@ -6,26 +6,33 @@ use strict;
 use utf8;
 
 sub new {
-    my ($class, $fp, $opt) = @_;
+    my ($class, $file, $opt) = @_;
 
-    my ($did) = ($fp =~ /(\d+)\.xml/);
-    if ($opt->{gzipped}) {
-	open(READER, "zcat | $fp") or die;
+    my $did;
+    my $content;
+    if ((ref $file) eq 'SCALAR') {
+	$did = $opt->{did};
+	$content = $$file;
     } else {
-	open(READER, $fp) or die;
+	($did) = ($file =~ /(\d+)\.xml/);
+	if ($opt->{gzipped}) {
+	    open(READER, "zcat $file |") or die "$!";
+	} else {
+	    open(READER, $file) or die "$!";
+	}
+	binmode(READER, ':utf8');
+
+	while (<READER>) {
+	    $content .= $_;
+	}
+	close(READER);
     }
-    binmode(READER, ':utf8');
 
-    my $buf;
-    while (<READER>) {
-	$buf .= $_;
-    }
-    close(READER);
+    my $this = {did => $did, is_old_version => $opt->{is_old_version}};
+    $this = bless $this;
+    $this->parse($content);
 
-    my $this = {did => $did};
-    &parse($this, $buf);
-
-    bless $this;
+    return $this;
 }
 
 sub DESTROY {}
@@ -38,14 +45,19 @@ sub parse {
     my ($header) = ($xmldat =~ m!<Header>((.|\n)+?)</Header>!);
     my ($text) = ($xmldat =~ m!<Text[^>]+?>((.|\n)+?)</Text>!);
 
-    $this->{title} = &parseMetaInfo($header, "Title");
-    $this->{keywords} = &parseMetaInfo($header, "Keywords");
-    $this->{description} = &parseMetaInfo($header, "Description");
+    $this->{title} = $this->parseMetaInfo($header, "Title");
+    $this->{keywords} = $this->parseMetaInfo($header, "Keywords");
+    $this->{description} = $this->parseMetaInfo($header, "Description");
 
-    $this->{outlinks} = &parseLinkInfo($header, "OutLink");
-    $this->{inlinks} = &parseLinkInfo($header, "InLink");
+    $this->{outlinks} = $this->parseLinkInfo($header, "OutLink");
+    $this->{inlinks} = $this->parseLinkInfo($header, "InLink");
 
-    $this->{sentences} = &parseText($text);
+    $this->{sentences} = $this->parseText($text);
+}
+
+sub getID {
+    my ($this) = @_;
+    return $this->{id};
 }
 
 sub getInlinks {
@@ -95,52 +107,59 @@ sub getSentences {
 
 
 sub trim {
-    my ($annotation) = @_;
+    my ($this, $annotation) = @_;
 
     my @buf = split("\n", $annotation);
-    shift @buf;
-    my $hoge = pop @buf;
+    if ($this->{is_old_version}) {
+	my $line = shift @buf;
+	$line =~ s/<!\[CDATA\[//;
+	unshift(@buf, $line);
+    } else {
+	shift @buf;
+    }
+    pop @buf;
 
     return join("\n", @buf);
 }
 
 sub parseText {
-    my ($text) = @_;
+    my ($this, $text) = @_;
 
     my @sentences;
-    while ($text =~ m!<S[^>]+?>((.|\n)+)</S>!g) {
-	my $sentence = $1;
+    while ($text =~ m!<S[^>]+?Id="(\d+)">((.|\n)+?)</S>!g) {
+	my $sid = $1;
+	my $sentence = $2;
 	my ($rawstring) = ($sentence =~ m!<RawString>(.+?)</RawString>!);
 	my ($annotation) = ($sentence =~ m!<Annotation[^>]+>((.|\n)+?)</Annotation>!);
 
-	push(@sentences, { rawstring => $rawstring, annotation => &trim($annotation) });
+	push(@sentences, { id => $sid, rawstring => $rawstring, annotation => $this->trim($annotation) });
     }
 
     return \@sentences;
 }
 
 sub parseMetaInfo {
-    my ($header, $tagname) = @_;
+    my ($this, $header, $tagname) = @_;
 
     my $item;
-    if ($header =~ m!<$tagname[^>]*?>((.|\n)+)</$tagname>!) {
+    if ($header =~ m!<$tagname[^>]*?>((.|\n)+?)</$tagname>!) {
 	my $element = $1;
 
 	my ($rawstring) = ($element =~ m!<RawString>(.+?)</RawString>!);
 	my ($annotation) = ($element =~ m!<Annotation[^>]+>((.|\n)+?)</Annotation>!);
 
 	$item->{rawstring} = $rawstring;
-	$item->{annotation} = &trim($annotation);
+	$item->{annotation} = $this->trim($annotation);
     }
 
     return $item;
 }
 
 sub parseLinkInfo {
-    my ($header, $tagname) = @_;
+    my ($this, $header, $tagname) = @_;
 
     my @links;
-    while ($header =~ m!<$tagname>((.|\n)+)</$tagname>!g) {
+    while ($header =~ m!<$tagname>((.|\n)+?)</$tagname>!g) {
 	my $link = $1;
 	my ($rawstring) = ($link =~ m!<RawString>(.+?)</RawString>!);
 	my ($annotation) = ($link =~ m!<Annotation[^>]+>((.|\n)+?)</Annotation>!);
@@ -153,59 +172,12 @@ sub parseLinkInfo {
 	}
 
 	push(@links, { rawstring => $rawstring,
-		       annotation => &trim($annotation),
+		       annotation => $this->trim($annotation),
 		       urls => \@urls,
 		       dids => \@dids });
     }
 
     return \@links;
 }
-
-
-# 	if (/\<$opt{extract_from}( |\>)/) {
-# 	    $contentFlag = 1;
-# 	}
-# 	elsif (/\<\/$opt{extract_from}\>/) {
-# 	    $contentFlag = 0;
-# 	}
-
-# 	if (/\<S.*? Id="(\d+)"/) {
-# 	    print STDERR "\rdir=$opt{in},file=$fid (Id=$1)" if ($opt{verbose});
-# 	    $sid = $1;
-# 	}
-# 	elsif (/\<(?:Title|InLink|OutLink|Description|Keywords)/) {
-# 	    $sid++;
-# 	    print STDERR "\rdir=$opt{in},file=$fid (Id=$sid)" if ($opt{verbose});
-# 	}
-
-
-# 	if (/^\]\]\><\/Annotation>/) {
-# 	    unless ($result =~ /^\n*$/) {
-# 		if ($opt{syn}) {
-# 		    $indice{$sid} = $indexer->makeIndexfromSynGraph4Indexing($result);
-# 		}
-# 		elsif ($opt{knp}) {
-# 		    $indice{$sid} = $indexer->makeIndexFromKNPResult($result, \%opt);
-# 		}
-# 		else {
-# 		    $indice{$sid} = $indexer->makeIndexfromJumanResult($result);
-# 		}
-# 	    }
-
-# 	    $result = undef;
-# 	    $annotationFlag = 0;
-# 	} elsif (/.*\<Annotation Scheme=\"$opt{scheme}\"\>\<\!\[CDATA\[/) {
-# 	    my $line = "$'";
-# 	    $result = $line unless ($line =~ /^#/);
-# 	    $annotationFlag = 1;
-# 	} elsif ($annotationFlag && $contentFlag) {
-# 	    next if ($_ =~ /^\#/);
-
-# 	    if ($opt{scheme} eq 'SynGraph' && $opt{knp}) {
-# 		next if ($_ =~ /^!/);
-# 	    }
-# 	    $result .= $_;
-# 	}
-#     }
 
 1;
