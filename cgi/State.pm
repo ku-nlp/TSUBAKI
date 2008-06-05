b7b397d36d912841fc68bb31df1151a3a86a5ab7
Keiji Shinzato <skeiji@(null)>
2008-06-05 19:06:53 +0000

    added.

---
+++ cgi/State.pm
@@ -0,0 +1,48 @@
+package State;
+
+use strict;
+use utf8;
+use Configure;
+
+my $CONFIG = Configure::get_instance();
+
+sub new {
+    my ($class) = @_;
+    my $this = {
+	checkIn => 0,
+	checkOutCalled => 0
+    };
+
+    bless $this;
+}
+
+sub DESTROY {
+    my ($this) = @_;
+
+    unlink($CONFIG->{LOCK_FILE}) if (!$this->{checkOutCalled} && $this->{checkIn});
+}
+
+sub checkIn {
+    my ($this) = @_;
+
+    my $count = 0;
+    while (!symlink($CONFIG->{DUMY_LOCK_FILE}, $CONFIG->{LOCK_FILE})) {
+	$count++;
+	sleep(1);
+	if ($count > 5) {
+	    return 0;
+	}
+    }
+
+    $this->{checkIn} = 1;
+    return 1;
+}
+
+sub checkOut {
+    my ($this) = @_;
+
+    unlink($CONFIG->{LOCK_FILE});
+    $this->{checkOutCalled} = 1;
+}
+
+1;
