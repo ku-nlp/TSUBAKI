a54582cdcbdcb504e309dcea85de21720b418699
Keiji Shinzato <skeiji@(null)>
2008-06-16 06:24:54 +0000

    バイナリ化処理を追加した.

--- scripts/merge-index.sh
+++ scripts/merge-index.sh
@@ -28,3 +28,22 @@ cd ..
 echo "perl -I $scriptdir $scriptdir/merge_sorted_idx.pl -dir $id -z -suffix idx.gz | gzip > $id.idx.gz"
 perl -I $scriptdir $scriptdir/merge_sorted_idx.pl -dir $id -z -suffix idx.gz | gzip > $id.idx.gz
 rm -r $id
+
+
+
+fname=$id.idx.gz
+
+# バイナリ化
+echo perl -I $scriptdir $scriptdir/binarize_idx.pl -z -quiet $fname
+perl -I $scriptdir $scriptdir/binarize_idx.pl -z -quiet $fname
+
+# 文書頻度の取得
+echo "perl $scriptdir/idx2df.pl $id.idx.gz"
+perl $scriptdir/idx2df.pl $id.idx.gz
+
+# 文書長データベースの作成
+echo "perl $scriptdir/make-dlength-db.perl -z $id.idx.gz"
+perl $scriptdir/make-dlength-db.perl -z $id.idx.gz 
+
+echo rm $fname
+rm $fname
