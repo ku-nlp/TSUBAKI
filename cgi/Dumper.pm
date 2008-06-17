9b4a9b634505c8f07e3d0bcac73da260fe63cbe5
Keiji Shinzato <skeiji@(null)>
2008-06-17 02:48:50 +0000

    added.

---
+++ cgi/Dumper.pm
@@ -0,0 +1,20 @@
+package Dumper;
+
+# $Id$
+
+use strict;
+use utf8;
+use Data::Dumper;
+
+sub dump_as_HTML {
+    my ($obj) = @_;
+
+    my $dumpstr = Dumper($obj);
+
+    $dumpstr =~ s/\n/<br>\n/g;
+    $dumpstr =~ s/ /&nbsp;/g;
+
+    return $dumpstr;
+}
+
+1;
