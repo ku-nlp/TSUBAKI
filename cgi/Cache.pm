602aa9fbcff192e2b9f953835af40badd6d55862
Keiji Shinzato <skeiji@(null)>
2008-07-16 10:28:47 +0000

    ヒット件数が0件のときは、キャッシュに保存しないようにした.

--- cgi/Cache.pm
+++ cgi/Cache.pm
@@ -34,16 +34,18 @@ sub new {
 sub save {
     my ($this, $query, $result) = @_;
 
-    my $timestamp = strftime("%Y-%m-%d-%T", localtime(time));
-    my $fname = $timestamp;
-    my $filepath = sprintf("%s/%s", $CONFIG->{CACHE_DIR}, $fname);
-    store($result, $filepath) or die;
+    if ($result->{hitcount} > 0) {
+	my $timestamp = strftime("%Y-%m-%d-%T", localtime(time));
+	my $fname = $timestamp;
+	my $filepath = sprintf("%s/%s", $CONFIG->{CACHE_DIR}, $fname);
+	store($result, $filepath) or die;
 
-    my $normalizedQuery = $query->normalize();
-    my $cachelog = "$CONFIG->{CACHE_DIR}/log";
-    open(WRITER, '>>:utf8', $cachelog) or die;
-    print WRITER "$timestamp\t$normalizedQuery\t$filepath\n";
-    close(WRITER);
+	my $normalizedQuery = $query->normalize();
+	my $cachelog = "$CONFIG->{CACHE_DIR}/log";
+	open(WRITER, '>>:utf8', $cachelog) or die;
+	print WRITER "$timestamp\t$normalizedQuery\t$filepath\n";
+	close(WRITER);
+    }
 }
 
 sub exists {
