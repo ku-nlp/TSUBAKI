9934054780790e6d8fa81c7d7b378e16a253c44f
Keiji Shinzato <skeiji@(null)>
2008-10-15 07:28:18 +0000

    added.

---
+++ scripts/access-log-collecter.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# $Id$
+
+# apache のログファイルを取得しつづけるデーモン
+
+# NICT
+if [ `domainname` = 'crawl.kclab.jgn2.jp' ]; then
+    CONFIG=configure.nict
+else
+    CONFIG=configure
+fi
+
+SLEEP_TIME=5
+
+hostname=`grep LOADBALANCER_NAME $CONFIG | awk '{print $2}'`
+access_log=`grep APACHE_ACCESS_LOG $CONFIG | awk '{print $2}'`
+dist_dir=`grep DATA_DIR $CONFIG | awk '{print $2}'`
+
+while [ 1 ]
+do
+    scp $hostname:$access_log $dist_dir/ 2> /dev/null
+    sleep $SLEEP_TIME
+done
