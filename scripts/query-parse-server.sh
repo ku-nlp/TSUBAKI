1867d740adebb3913191e2a0a763356f208d520c
Keiji Shinzato <skeiji@(null)>
2009-02-02 05:59:25 +0000

    added.

---
+++ scripts/query-parse-server.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# $Id$
+
+# VERBOSE=-verbose
+VERBOSE=
+
+HOSTS_PREFIX=
+HOSTS_START=
+HOSTS_END=
+
+
+# NICT
+if [ `domainname` = 'crawl.kclab.jgn2.jp' ]; then
+    HOSTS_PREFIX=iccc00
+    HOSTS_START=3
+    HOSTS_END=4
+    TSUBAKI_DIR=$HOME/public_html/cgi-bin/SearchEngine-develop
+else
+    HOSTS_PREFIX=nlpc0
+    HOSTS_START=2
+    HOSTS_END=6
+    TSUBAKI_DIR=$HOME/tsubaki-develop/SearchEngine
+fi
+
+SCRIPTS_DIR=$TSUBAKI_DIR/scripts
+CGI_DIR=$TSUBAKI_DIR/cgi
+COMMAND=query_parse_server.pl
+NICE=-4
+PERL=$HOME/local/bin/perl
+
+
+start() {
+    for n in `seq $HOSTS_START $HOSTS_END`
+    do
+	h=$HOSTS_PREFIX$n
+	ssh -f $h "ulimit -Ss unlimited ; nice $NICE $PERL -I $CGI_DIR $SCRIPTS_DIR/$COMMAND"
+    done
+}
+
+status_or_stop() {
+    for n in `seq $HOSTS_START $HOSTS_END`
+    do
+	h=$HOSTS_PREFIX$n
+	pid=`ssh -f $h ps auxww | grep $COMMAND | grep -v grep | perl -lne "push(@list, \\$1) if /^$USER\s+(\d+)/; END {print join(' ', @list) if @list}"`
+ 	if [ "$1" = "stop" -a -n "$pid" ]; then
+ 	    ssh -f $h kill $pid
+ 	fi
+	echo "$h: $pid"
+    done
+}
+
+restart() {
+    status_or_stop stop
+    start
+}
+
+case $1 in
+    start)
+	start
+	;;
+    stop)
+	status_or_stop stop
+	;;
+    restart)
+	restart
+	;;
+    status)
+	status_or_stop status
+	;;
+    *)
+	echo "$0 start|stop|restart|status"
+	exit 1
+esac
+exit 0
---
+++ scripts/query_parse_server.pl
@@ -0,0 +1,120 @@
+#!/usr/bin/env perl
+
+# $Id$
+
+# クエリ処理用サーバー
+
+use strict;
+use utf8;
+use Encode;
+use MIME::Base64;
+use IO::Socket;
+use Storable;
+use Logger;
+use Configure;
+use QueryParser;
+use Data::Dumper;
+{
+    package Data::Dumper;
+    sub qquote { return shift; }
+}
+$Data::Dumper::Useperl = 1;
+
+binmode(STDOUT, ':encoding(euc-jp)');
+binmode(STDERR, ':encoding(euc-jp)');
+
+
+
+my $HOSTNAME = `hostname | cut -f 1 -d .` ; chop($HOSTNAME);
+
+my $CONFIG = Configure::get_instance();
+
+&main();
+
+sub main {
+    my $listening_socket = IO::Socket::INET->new(
+	LocalPort => $CONFIG->{PORT_OF_QUERY_PARSE_SERVER},
+	Listen    => SOMAXCONN,
+	Proto     => 'tcp',
+	Reuse     => 1);
+
+    unless ($listening_socket) {
+	my $host = `hostname`; chop($host);
+	die "Cannot listen port No. " . $CONFIG->{PORT_OF_QUERY_PARSE_SERVER} . " on $host. $!\n";
+	exit;
+    }
+
+    push(@INC, $CONFIG->{SYNGRAPH_PM_PATH});
+    require SynGraph;
+
+    my $SYNGRAPH = new SynGraph($CONFIG->{SYNDB_PATH});
+    while (1) {
+	my $new_socket = $listening_socket->accept();
+	my $client_sockaddr = $new_socket->peername();
+	my ($client_port,$client_iaddr) = unpack_sockaddr_in($client_sockaddr);
+	my $client_hostname = gethostbyaddr($client_iaddr, AF_INET);
+	my $client_ip = inet_ntoa($client_iaddr);
+	
+	select($new_socket); $|=1; select(STDOUT);
+
+	my $pid;
+	if ($pid = fork()) {
+	    $new_socket->close();
+	    wait;
+	} else {
+	    select($new_socket); $|=1; select(STDOUT);
+
+	    my $logger = new Logger();
+	    my $buff;
+	    while (<$new_socket>) {
+		last if ($_ eq "EOD\n");
+		$buff .= $_;
+	    }
+	    my $params = Storable::thaw(decode_base64($buff));
+
+	    my $q_parser = new QueryParser({
+		DFDB_DIR => $CONFIG->{SYNGRAPH_DFDB_PATH},
+		ignore_yomi => $CONFIG->{IGNORE_YOMI},
+		use_of_case_analysis => $params->{use_of_case_analysis},
+		use_of_NE_tagger => $params->{use_of_NE_tagger},
+		debug => $params->{debug},
+		option => $params });
+
+	    # クエリの解析
+	    # logical_cond_qk: クエリ間の論理演算
+	    my $query = $q_parser->parse(
+		$params->{query},
+		{ logical_cond_qk => $params->{logical_operator},
+		  syngraph => $params->{syngraph},
+		  near => $params->{near},
+		  force_dpnd => $params->{force_dpnd},
+		  trimming => $params->{trimming},
+		  antonym_and_negation_expansion => $params->{antonym_and_negation_expansion},
+		  detect_requisite_dpnd => $params->{detect_requisite_dpnd},
+		  query_filtering => $params->{query_filtering},
+		  disable_dpnd => $params->{disable_dpnd},
+		  disable_synnode => $params->{disable_synnode},
+		  telic_process => $params->{telic_process},
+		  CN_process => $params->{CN_process},
+		  NE_process => $params->{NE_process},
+		  modifier_of_NE_process => $params->{modifier_of_NE_process},
+		  logger => $logger,
+		  SYNGRAPH => $SYNGRAPH,
+		  debug => $params->{debug}
+		});
+	
+
+	    # sending log data in a search slave server.
+	    print $new_socket encode_base64(Storable::freeze($logger), "") , "\n";
+	    print $new_socket "EOL\n";
+
+	    $query->{option}{SYNGRAPH} = undef;
+
+	    print $new_socket encode_base64(Storable::freeze($query), "") , "\n";
+	    print $new_socket "EOD\n";
+
+	    $new_socket->close();
+	    exit;
+	}
+    }
+}
