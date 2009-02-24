bb4f4c63d6defda1af8764ad9a9c9fb9a1a2a18a
Keiji Shinzato <skeiji@(null)>
2009-02-24 13:30:58 +0000

    tsubaki_server.plが動作しているかどうか監視し、停止しているようであれば再起動するようにした.

--- scripts/tsubaki-server.sh
+++ scripts/tsubaki-server.sh
@@ -23,8 +23,8 @@ else
     HOSTS_PREFIX=nlpc
     HOSTS_START=34
     HOSTS_END=59
+    TSUBAKI_DIR=$HOME/tsubaki/SearchEngine
     DEGIT=2
-    TSUBAKI_DIR=$HOME/tsubaki-develop/SearchEngine
 fi
 
 SCRIPTS_DIR=$TSUBAKI_DIR/scripts
@@ -47,64 +47,31 @@ else
 fi
 
 
-
-start() {
+call() {
     for n in `seq $HOSTS_START $HOSTS_END | awk '{printf "%0'$DEGIT'd\n", $1}'`
     do
-	while read LINE
-	do
-	    if echo "$LINE" | grep -E '^[^\#]' >/dev/null 2>&1; then
-		h=$HOSTS_PREFIX$n
-		idxdir=`echo $LINE | cut -f 1 -d ' '`
-		anchor_idxdir=`echo $LINE | cut -f 2 -d ' '`
-		port=`echo $LINE | cut -f 3 -d ' '`
-		dlengthdbdir=$idxdir
-		OPTION="-idxdir $idxdir -dlengthdbdir $dlengthdbdir -port $port $USE_OF_SYNGRAPH $VERBOSE"
-		if [ $anchor_idxdir != none ]; then
-		    OPTION=$OPTION" -idxdir4anchor $anchor_idxdir"
-		fi
-
-	        ssh -f $h "nice $NICE $PERL -I $CGI_DIR -I $SCRIPTS_DIR -I $MODULE_DIR -I $UTILS_DIR $SCRIPTS_DIR/$COMMAND $OPTION"
-	    fi
-	done < $PORTSFILE
-    done
-}
-
-status_or_stop() {
-    for n in `seq $HOSTS_START $HOSTS_END | awk '{printf "%0'$DEGIT'd\n", $1}'`
-    do
-	while read LINE
-	do
-	    if echo "$LINE" | grep -E '^[^\#]' >/dev/null 2>&1; then
-		h=$HOSTS_PREFIX$n
-		PORT=`echo $LINE | cut -f 3 -d ' '`
-		pid=`ssh -f $h ps auxww | grep $COMMAND | grep $PORT | grep -v grep | perl -lne "push(@list, \\$1) if /^$USER\s+(\d+)/; END {print join(' ', @list) if @list}"`
- 		if [ "$1" = "stop" -a -n "$pid" ]; then
- 		    ssh -f $h kill $pid
- 		fi
-		echo "$h: $pid"
-	    fi
-	done < $PORTSFILE
+	h=$HOSTS_PREFIX$n
+	ssh -f $h "sh $SCRIPTS_DIR/tsubaki-server-manager.sh $1"
     done
 }
 
 restart() {
-    status_or_stop stop
+    status_or_stop_or_halt halt
     start
 }
 
 case $1 in
     start)
-	start
+	call start
 	;;
     stop)
-	status_or_stop stop
+	call halt
 	;;
     restart)
-	restart
+	call restart
 	;;
     status)
-	status_or_stop status
+	call status
 	;;
     *)
 	echo "$0 start|stop|restart|status"
