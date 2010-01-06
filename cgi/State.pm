b464061f810e462d362a97f6bf2ada411a077430
Keiji Shinzato <skeiji@(null)>
2010-01-06 01:06:12 +0000

    古い LOCK_FILE を削除するように.

--- cgi/State.pm
+++ cgi/State.pm
@@ -26,11 +26,23 @@ sub checkIn {
     my ($this) = @_;
 
     my $count = 0;
-    while (!symlink($CONFIG->{DUMY_LOCK_FILE}, $CONFIG->{LOCK_FILE})) {
-	$count++;
-	sleep(1);
-	if ($count > $CONFIG->{MAX_NUM_OF_RETRY} && $CONFIG->{MAX_NUM_OF_RETRY} > 0) {
-	    return 0;
+    if (!symlink($CONFIG->{DUMY_LOCK_FILE}, $CONFIG->{LOCK_FILE})) {
+	require File::stat;
+
+	my $stats = stat($ARGV[0]);
+	my $diff = time - $stats->mtime;
+
+	# 作成後300秒以上経過している場合は削除
+	if ($diff > 300) {
+	    unlink ($CONFIG->{LOCK_FILE});
+	}
+
+	while (!symlink($CONFIG->{DUMY_LOCK_FILE}, $CONFIG->{LOCK_FILE})) {
+	    $count++;
+	    sleep(1);
+	    if ($count > $CONFIG->{MAX_NUM_OF_RETRY} && $CONFIG->{MAX_NUM_OF_RETRY} > 0) {
+		return 0;
+	    }
 	}
     }
 
