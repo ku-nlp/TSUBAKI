e27c4341a77194dcbb67b8f17bf4fa2515b640a1
Keiji Shinzato <skeiji@(null)>
2010-04-28 09:06:41 +0000

    ブロックタイプを除いてマージするオプションの追加.

--- scripts/idx2df.pl
+++ scripts/idx2df.pl
@@ -1,30 +1,68 @@
 #!/usr/bin/env perl
 
+# $Id$
+
 # usage: idx2df.pl 00.idx 01.idx.gz
 
 use strict;
 use utf8;
 use Encode;
+use Getopt::Long;
+use Encode;
 
-# binmode(STDIN, ":encoding(utf8)");
-# binmode(STDOUT, ":encoding(utf8)");
+my (%opt);
+GetOptions(\%opt, 'remove_tag', 'debug');
 
 foreach my $fp (@ARGV){
-    if($fp =~ /^(.+)\.gz$/){
-	open(READER, "zcat $fp | ") or die;
-	open(WRITER, "> $1.df") or die;
-    }else{
-	open(READER, $fp) or die;
-	open(WRITER, "> $fp.df") or die;
+    my $outf = $fp;
+    if ($fp =~ /^(.+)\.gz$/) {
+	$outf = $1;
+	open (READER, "zcat $fp | ") or die $!;
+    } else {
+	open (READER, $fp) or die $!;
     }
 
-    while(<READER>){
-	chop($_);
-	my($word, @docs) = split(/ /, decode('utf8', $_));
-	my $size = scalar(@docs);
-	$word = encode('utf8', $word);
-	print WRITER "$word $size\n";
+    if ($opt{debug}) {
+	open (WRITER, ">&STDOUT");
+    } else {
+	open (WRITER, "> $outf.df") or die $!;
     }
+
+    binmode (READER, ':utf8');
+    binmode (WRITER, ':utf8');
+
+    my %buff = ();
+    my $prev = undef;
+    while(<READER>){
+	chop;
+
+	my ($term, @docs) = split(/ /, $_);
+	if ($opt{remove_tag}) {
+	    my ($tag, $_term) = ($term =~ /^(.*?):(.+)$/);
+	    if (defined $prev && $prev ne $_term) {
+		my $size = scalar(keys %buff);
+		print WRITER "$prev $size\n";
+		%buff = ();
+	    }
+	    $prev = $_term;
+
+	    foreach my $doc (@docs) {
+		my ($did, $dat) = split (/:/, $doc);
+		$buff{$did} = 1;
+	    }
+	}
+	else {
+	    my $size = scalar (@docs);
+	    print WRITER "$term $size\n";
+	}
+    }
+
+    if ($opt{remove_tag}) {
+	my $size = scalar(keys %buff);
+	print WRITER "$prev $size\n";
+    }
+
+
     close(READER);
     close(WRITER);
 }
