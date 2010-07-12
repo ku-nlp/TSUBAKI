2004159415793db53ac756f962f66f6dbcc78deb
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2010-07-12 10:53:32 +0000

    Changed the default value of C_PAGERANK to 10.

--- search/common.h
+++ search/common.h
@@ -31,7 +31,7 @@
 #define MAX_LENGTH_OF_DOCUMENT 3
 #define SIZEOFINT sizeof(int)
 #define NUM_OF_RETURN_DOCUMENTS 30
-#define C_PAGERANK 100
+#define C_PAGERANK 10
 #define WEIGHT_OF_TSUBAKI_SCORE 0.5
 
 enum documents_type {
