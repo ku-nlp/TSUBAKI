bf9f1856d2c5abd93692a6d9198226a3cf591462
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2010-10-26 12:34:39 +0000

    Added english mode.

--- sf2index/search.sh
+++ sf2index/search.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 usage() {
-    echo "$0 query_string"
+    echo "$0 [-e] query_string"
+    echo "\t-e: english mode"
     exit 1
 }
+
+OtherOptions=
+while getopts eh OPT
+do
+    case $OPT in
+        e)  OtherOptions=-english
+            ;;
+        h)  usage
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
 
 query=$1
 UtilsDir=../../Utils
@@ -12,4 +25,4 @@ if [ -z "$query" ]; then
     usage
 fi
 
-perl -I $UtilsDir/perl -I ../scripts -I ../cgi -I ../perl ../scripts/tsubaki_test.pl -idxdir . -dlengthdbdir . -disable_query_processing -query $query
+perl -I $UtilsDir/perl -I ../scripts -I ../cgi -I ../perl ../scripts/tsubaki_test.pl -idxdir . -dlengthdbdir . -disable_query_processing $OtherOptions -query $query
