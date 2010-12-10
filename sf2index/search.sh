7df10aa66f91c4f3a7e7146fe0b5fbd9e7703646
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2010-12-10 01:23:27 +0000

    Modified to search by a given query.

--- sf2index/search.sh
+++ sf2index/search.sh
@@ -1,28 +1,48 @@
 #!/bin/sh
 
 usage() {
-    echo "$0 [-e] query_sexp_file"
+    echo "$0 [-e] query"
+    echo "\t-d dir: specify index_dir"
     echo "\t-e: english mode"
+    echo "\t-f: specify query_file instead of query"
     exit 1
 }
 
-OtherOptions=
-while getopts eh OPT
+searchengine_dir=$HOME/share/tool/SearchEngine
+utils_dir=$HOME/share/tool/Utils
+
+file_mode=0
+query_options=
+data_dir=sample_data
+while getopts d:efh OPT
 do
     case $OPT in
-        e)  OtherOptions=-english
+	d)  data_dir=$OPTARG
+	    ;;
+        e)  query_options=-english
             ;;
+	f)  file_mode=1
+	    ;;
         h)  usage
             ;;
     esac
 done
 shift `expr $OPTIND - 1`
 
-query=$1
-datadir=sample_data
+if [ $file_mode -eq 0 ]; then
+    query=$1
+    if [ -z "$query" ]; then
+	usage
+    fi
 
-if [ -z "$query" -o ! -f "$query" ]; then
-    usage
+    query_file=tmp_query_sexp_$$
+    perl -I $utils_dir/perl -I $searchengine_dir/cgi -I $searchengine_dir/perl -I $searchengine_dir/scripts $searchengine_dir/scripts/query-sexp-generate.perl --syngraph --query $query $query_options > $query_file
+else
+    query_file=$1
 fi
 
-../search/slave_server $datadir $datadir 39999 `hostname` -standalone < $query
+../search/slave_server $data_dir $data_dir 39999 `hostname` -standalone < $query_file
+
+if [ $file_mode -eq 0 ]; then
+    rm -f $query_file
+fi
