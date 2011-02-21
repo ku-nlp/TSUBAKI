ef55b241d61b6fab92e2af5a685edf5b7c1dd267
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2011-02-21 14:24:57 +0000

    Modified to use StandardFormat.pm.

--- sf2index/sf2term.pl
+++ sf2index/sf2term.pl
@@ -8,6 +8,7 @@ use strict;
 use utf8;
 use Getopt::Long;
 use XML::LibXML;
+use StandardFormat;
 
 our %opt;
 &GetOptions(\%opt, 'suffix=s');
@@ -15,6 +16,7 @@ our %opt;
 our $SF_EXT = 'xml';
 our $IDX_EXT = $opt{suffix} ? $opt{suffix} : 'idx';
 our $parser = new XML::LibXML;
+our $sf = new StandardFormat;
 
 my $file = $ARGV[0];
 my ($prefix, $doc_id) = ($file =~ /^(.*?)([-\d]+)\.$SF_EXT$/);
@@ -35,56 +37,34 @@ sub xml2term {
     my $doc = $parser->parse_string($xmldat);
 
     my (%terms, %dpnd_terms);
-    my $word_count = 0;
     for my $sentence_node ($doc->getElementsByTagName('S')) { # sentence loop
 	my $sentence_id = $sentence_node->getAttribute('id');
-	my (%phrases);
 	for my $annotation_node ($sentence_node->getElementsByTagName('Annotation')) { # parse
-	    for my $phrase_node ($annotation_node->getElementsByTagName('phrase')) {
-		my (@words);
-		for my $word_node ($phrase_node->getElementsByTagName('word')) {
-		    my $str = $word_node->getAttribute('str'); # word terms (string that appeared)
-		    $str .= '*' if $str;
-		    my $lem = $word_node->getAttribute('lem'); # word terms (lem)
-		    for my $term ($str, $lem) {
-			next unless $term;
-			&hash_term(\%terms, $term, $sentence_id, $word_count, 1); # score=1
-		    }
-		    push(@words, {str => $str, lem => $lem, 
-				  feature => $word_node->getAttribute('feature'), 
-				  pos => $word_count});
-		    $word_count++;
+	    $sf->read_annotation_from_node($annotation_node);
 
-		    for my $synnode ($word_node->getElementsByTagName('synnode')) { # synonym node
-			my $synid = $synnode->getAttribute('synid');
-			my $score = $synnode->getAttribute('score');
-			next unless $score < 1; # except the identical expression with the word
-			my $wordid = (split(',', $synnode->getAttribute('wordid')))[0]; # the first wordid
-			&hash_term(\%terms, $synid, $sentence_id, $wordid, $score);
-		    }
+	    for my $id (sort {$sf->{words}{$a}{pos} <=> $sf->{words}{$b}{pos}} keys %{$sf->{words}}) { # order: left-to-right
+		my $word = $sf->{words}{$id};
+		for my $term ($word->{str}, $word->{lem}) {
+		    next unless $term;
+		    &hash_term(\%terms, $term, $sentence_id, $word->{pos}, 1); # score=1
 		}
-		my $id = $phrase_node->getAttribute('id');
-		my $word_head_num = &get_phrase_head_num(\@words);
-		$phrases{$id} = {head_ids => [split('/', $phrase_node->getAttribute('head'))], 
-				 words => \@words, 
-				 word_head_num => $word_head_num, # head word in this phrase
-				 str => $words[$word_head_num]{str}, 
-				 lem => $words[$word_head_num]{lem}, 
-				 pos => $words[$word_head_num]{pos}, 
-				};
+		for my $synnode (@{$word->{synnodes}}) { # synonym node
+		    next unless $synnode->{score} < 1; # except the identical expression with the word
+		    my $wordid = (split(',', $synnode->{wordid}))[0]; # the first wordid
+		    &hash_term(\%terms, $synnode->{synid}, $sentence_id, $wordid, $synnode->{score});
+		}
 	    }
-	}
 
-	# dpnd terms
-	for my $id (sort {$phrases{$a} <=> $phrases{$b}} keys %phrases) {
-	    next if !$phrases{$id}{head_ids}; # skip roots of English (undef)
-	    for my $head_id (@{$phrases{$id}{head_ids}}) {
-		next if $head_id == -1; # skip roots of Japanese (-1)
-		my $dpnd_str = sprintf('%s->%s', $phrases{$id}{str}, $phrases{$head_id}{str}); # string that appeared
-		my $dpnd_lem = sprintf('%s->%s', $phrases{$id}{lem}, $phrases{$head_id}{lem}); # lem
-		for my $dpnd_term ($dpnd_str, $dpnd_lem) {
-		    next if $dpnd_term eq '->';
-		    &hash_term(\%dpnd_terms, $dpnd_term, $sentence_id, $phrases{$id}{pos}, 1); # score=1
+	    for my $id (sort {$sf->{phrases}{$a} <=> $sf->{phrases}{$b}} keys %{$sf->{phrases}}) {
+		next if !$sf->{phrases}{$id}{head_ids}; # skip roots of English (undef)
+		for my $head_id (@{$sf->{phrases}{$id}{head_ids}}) {
+		    next if $head_id == -1; # skip roots of Japanese (-1)
+		    my $dpnd_str = sprintf('%s->%s', $sf->{phrases}{$id}{str}, $sf->{phrases}{$head_id}{str}); # string that appeared
+		    my $dpnd_lem = sprintf('%s->%s', $sf->{phrases}{$id}{lem}, $sf->{phrases}{$head_id}{lem}); # lem
+		    for my $dpnd_term ($dpnd_str, $dpnd_lem) {
+			next if $dpnd_term eq '->';
+			&hash_term(\%dpnd_terms, $dpnd_term, $sentence_id, $sf->{phrases}{$id}{pos}, 1); # score=1
+		    }
 		}
 	    }
 	}
@@ -116,15 +96,4 @@ sub register_terms {
 
 	printf $OUTPUT "%s %s:%.2f@%s#%s\n", $term, $doc_id, $terms_hr->{$term}{score}, join (',', sort {$a <=> $b} keys %{$terms_hr->{$term}{sentence_ids}}), join ',', @buf;
     }
-}
-
-sub get_phrase_head_num {
-    my ($words_ar) = @_;
-
-    for my $i (0 .. $#{$words_ar}) {
-	if ($words_ar->[$i]{feature} =~ /<(?:準)?内容語>/) {
-	    return $i;
-	}
-    }
-    return -1;
 }
