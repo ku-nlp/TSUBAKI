036bdf62e0a1544865aa742da031938dca9ce755
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2011-02-23 11:41:10 +0000

    Modified directory settings.

--- enju2tsubaki/run.sh
+++ enju2tsubaki/run.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-enju -A -so < $1 > $1.enju.so
-perl enjuquery.pl $1.enju.so > $1.tsubaki1.so
-./StandOffManager/som export $1.tsubaki1.so $1 $1.tsubaki1.xml
-perl addrawtext.pl $1.tsubaki1.xml > $1.tsubaki.xml
+dir=$HOME/share/tool/SearchEngine/enju2tsubaki
 
+usage() {
+    echo "Usage: $0 input.txt"
+    exit 1
+}
+
+if [ -z "$1" -o ! -f "$1" ]; then
+    usage
+fi
+
+enju -A -so < $1 > $1.enju.so
+perl -I $dir $dir/enjuquery.pl $1.enju.so > $1.tsubaki1.so
+$dir/StandOffManager/som export $1.tsubaki1.so $1 $1.tsubaki1.xml
+perl $dir/addrawtext.pl $1.tsubaki1.xml
