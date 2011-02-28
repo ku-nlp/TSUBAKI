147a52a9726d7c96761865d72271b21dd2f404d4
Keiji Shinzato <skeiji@(null)>
2011-02-28 06:32:02 +0000

    small changes.

--- scripts/query_parse_server.pl
+++ scripts/query_parse_server.pl
@@ -72,7 +72,7 @@ sub main {
 		$buff .= $_;
 	    }
 	    my $params = Storable::thaw(decode_base64($buff));
-
+	    
 	    my $q_parser = new QueryParser({
 		DFDB_DIR => $CONFIG->{SYNGRAPH_DFDB_PATH},
 		ignore_yomi => $CONFIG->{IGNORE_YOMI},
@@ -81,30 +81,13 @@ sub main {
 		debug => $params->{debug},
 		option => $params });
 
+	    # set parameters
+	    $params->{logger} = $logger;
+	    $params->{logical_cond_qk} = $params->{logical_operator};
+
 	    # クエリの解析
 	    # logical_cond_qk: クエリ間の論理演算
-	    my $query = $q_parser->parse(
-		$params->{query},
-		{ logical_cond_qk => $params->{logical_operator},
-		  syngraph => $params->{syngraph},
-		  near => $params->{near},
-		  force_dpnd => $params->{force_dpnd},
-		  trimming => $params->{trimming},
-		  antonym_and_negation_expansion => $params->{antonym_and_negation_expansion},
-		  detect_requisite_dpnd => $params->{detect_requisite_dpnd},
-		  query_filtering => $params->{query_filtering},
-		  disable_dpnd => $params->{disable_dpnd},
-		  disable_synnode => $params->{disable_synnode},
-		  telic_process => $params->{telic_process},
-		  CN_process => $params->{CN_process},
-		  NE_process => $params->{NE_process},
-		  modifier_of_NE_process => $params->{modifier_of_NE_process},
-		  logger => $logger,
-		  SYNGRAPH => $SYNGRAPH,
-		  site => $params->{site},
-		  blockTypes => $params->{blockTypes},
-		  debug => $params->{debug}
-		});
+	    my $query = $q_parser->parse($params->{query}, $params);
 
 	    # sending log data in a search slave server.
 	    print $new_socket encode_base64(Storable::nfreeze($logger), "") , "\n";
