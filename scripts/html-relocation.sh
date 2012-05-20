76b1a1d31e006112bf1df79d0573a760a677df46
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2012-05-20 07:45:14 +0000

    Added verbose option.

--- scripts/html-relocation.sh
+++ scripts/html-relocation.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # indirからfindしたHTMLファイルを対象に、10桁IDのファイル名にコピーし、1ディレクトリ10000個ずつ格納する
+# -n : dryrun
+# -v : verbose
 
 # 出力:
 # outdir/000000/0000000000.html
@@ -12,20 +14,24 @@
 # ：
 
 usage() {
-    echo "Usage: $0 [-e file_extention] [-n] indir outdir"
+    echo "Usage: $0 [-e file_extention] [-n] [-v] indir outdir"
     exit 1
 }
 
 EXT=html
 NUM_OF_HTMLS_IN_DIR=10000
 DRYRUN=
+COMMAND=cp
+COMMAND_ARGS=
 
-while getopts e:nh OPT
+while getopts e:nvh OPT
 do
     case $OPT in
 	e)  EXT=$OPTARG
 	    ;;
 	n)  DRYRUN="echo "
+	    ;;
+	v)  COMMAND_ARGS=-v
 	    ;;
         h)  usage
             ;;
@@ -48,7 +54,8 @@ fi
 
 dcount=0
 dstr=`printf %06d $dcount`
-outdir=$outtopdir/$dstr
+dstr4=`expr $dstr : "^\([0-9][0-9][0-9][0-9]\)"`
+outdir=$outtopdir/$dstr4/$dstr
 if [ ! -d $outdir ]; then
     ${DRYRUN}mkdir -p $outdir
 fi
@@ -57,13 +64,14 @@ hcount=0
 for f in `find $indir -name "*.$EXT"`
 do
     destf=$outdir/$dstr`printf %04d $hcount`.$EXT
-    ${DRYRUN}cp -v $f $destf
+    ${DRYRUN}$COMMAND $COMMAND_ARGS $f $destf
 
     hcount=`expr $hcount + 1`
     if [ $hcount -eq $NUM_OF_HTMLS_IN_DIR ]; then
 	dcount=`expr $dcount + 1`
 	dstr=`printf %06d $dcount`
-	outdir=$outtopdir/$dstr
+	dstr4=`expr $dstr : "^\([0-9][0-9][0-9][0-9]\)"`
+	outdir=$outtopdir/$dstr4/$dstr
 	if [ ! -d $outdir ]; then
 	    ${DRYRUN}mkdir -p $outdir
 	fi
