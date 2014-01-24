14f7e01f977a62ef33a355cca6e020f4f241f8c1
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2014-01-24 20:07:19 +0000

    added.

---
+++ sf2index/sf2term.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# $Id$
+
+ARGS=
+INDIR=
+OUTDIR=
+XML_SUFFIX=xml
+IDX0_SUFFIX=idx0
+BASEDIR=
+while getopts a:i:o:s:b:x: OPT
+do
+    case $OPT in
+	a)  ARGS=$OPTARG
+	    ;;
+	i)  INDIR=$OPTARG
+	    ;;
+	o)  OUTDIR=$OPTARG
+	    ;;
+	b)  BASEDIR=$OPTARG
+	    ;;
+	s)  XML_SUFFIX=$OPTARG
+	    ;;
+	x)  IDX0_SUFFIX=$OPTARG
+	    ;;
+        h)  usage
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+for f in $INDIR/*.$XML_SUFFIX; do
+    basename=`basename $f .$XML_SUFFIX`
+    perl -I$BASEDIR/cgi sf2term.pl $ARGS $f > $OUTDIR/$basename.$IDX0_SUFFIX
+done
