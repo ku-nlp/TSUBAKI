e32ad42e04c26f32df250b9deed713f48cb6c5c3
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2017-07-19 19:18:23 +0900

    python3に対応

--- samplecode/python_sample.py
+++ samplecode/python_sample.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import codecs
-import urllib2
+# sixが必要 (pip install six)
 
+from __future__ import print_function
+import sys
+import six
+import io
+import codecs
+from six.moves.urllib.parse import urlparse, urlencode, quote
+from six.moves.urllib.request import urlopen
+from six.moves.urllib.error import HTTPError
+
+# このアドレスを変更して下さい
+base_url = "http://tsubaki.ixnlp.nii.ac.jp/cgi/api.cgi"
+    
 def main():
-    query = u"京都の観光名所"
-    uri_escaped_query = urllib2.quote(query.encode("utf-8"))
+    query = u"京大"
+    uri_escaped_query = quote(query.encode("utf-8"))
     results = 20
     start = 1
-
-    base_url = "http://tsubaki.ixnlp.nii.ac.jp/api.cgi"
-    
+   
     # リクエストURLを作成
     req_url = "%s?query=%s&results=%d&start=%d" % (base_url, uri_escaped_query, results, start)
 
     try:
-        r = urllib2.urlopen(req_url)
+        r = urlopen(req_url)
         response = r.read()
-        print response.decode("utf-8")
-    except urllib2.HTTPError, e:
-        print e.code
-        print e.reason
+        print(response.decode("utf-8"))
+    except HTTPError as e:
+        print(e.code)
+        print(e.reason)
 
-        print e.read()
+        print(e.read())
     
 if __name__ == "__main__":
-    sys.stdin  = codecs.getreader('UTF-8')(sys.stdin)
-    sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+    if six.PY3:
+        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    else:
+        sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
 
     main()
 
